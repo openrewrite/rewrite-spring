83b8b13cf66b4f7a8ac64382c6d9084303f3e4b6
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-03-05 12:21:37 -0600

    Revert "revert publishing to circleci (#39)"
    This reverts commit ed686bb1ab45e97ecb96f2a5885d8b37b78faecb.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,64 @@
 import com.github.jk1.license.LicenseReportExtension
-import io.spring.gradle.bintray.SpringBintrayExtension
 import nebula.plugin.contacts.Contact
 import nebula.plugin.contacts.ContactsExtension
 import nebula.plugin.info.InfoBrokerPlugin
 import nl.javadude.gradle.plugins.license.LicenseExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
-import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
 import java.util.*
 
 buildscript {
     repositories {
-        jcenter()
         gradlePluginPortal()
-    }
-
-    dependencies {
-        classpath("io.spring.gradle:spring-release-plugin:0.20.1")
-
-        constraints {
-            classpath("org.jfrog.buildinfo:build-info-extractor-gradle:4.13.0") {
-                because("Need recent version for Gradle 6+ compatibility")
-            }
-        }
     }
 }
 
 plugins {
     `java-library`
-    id("org.jetbrains.kotlin.jvm") version "1.4.21"
-    id("io.spring.release") version "0.20.1"
-    id("com.github.jk1.dependency-license-report") version "1.16"
-}
+    `maven-publish`
+    signing
 
-//apply(plugin = "license")
-apply(plugin = "nebula.maven-resolved-dependencies")
-apply(plugin = "io.spring.publishing")
+    id("nebula.maven-resolved-dependencies") version "17.3.2"
+    id("nebula.release") version "15.3.1"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+
+    id("com.github.hierynomus.license") version "0.15.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.4.21"
+    id("com.github.jk1.dependency-license-report") version "1.16"
+
+    id("nebula.maven-publish") version "17.3.2"
+    id("nebula.contacts") version "5.1.0"
+    id("nebula.info") version "9.3.0"
+
+    id("nebula.javadoc-jar") version "17.3.2"
+    id("nebula.source-jar") version "17.3.2"
+    id("nebula.maven-apache-license") version "17.3.2"
+}
 
 group = "org.openrewrite.recipe"
 description = "Eliminate legacy Spring patterns and migrate between major Spring Boot versions. Automatically."
 
 repositories {
     mavenLocal()
-    maven { url = uri("https://dl.bintray.com/openrewrite/maven") }
     mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME"))
+            password.set(project.findProperty("ossrhToken") as String? ?: System.getenv("OSSRH_TOKEN"))
+        }
+    }
+}
+
+signing {
+    setRequired({
+        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
+    })
+    val signingKey = project.findProperty("signingKey") as String? ?: System.getenv("SIGNING_KEY")
+    val signingPassword = project.findProperty("signingPassword") as String? ?: System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["nebula"])
 }
 
 configurations.all {
@@ -134,16 +149,16 @@ configure<ContactsExtension> {
 
     people["jkschneider@gmail.com"] = j
 }
-//
-//configure<LicenseExtension> {
-//    ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
-//    skipExistingHeaders = true
-//    header = project.rootProject.file("gradle/licenseHeader.txt")
-//    mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
-//    // exclude JavaTemplate shims from license check
-//    exclude("src/main/resources/META-INF/rewrite/*.java")
-//    strictCheck = true
-//}
+
+// configure<LicenseExtension> {
+//     ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
+//     skipExistingHeaders = true
+//     header = project.rootProject.file("gradle/licenseHeader.txt")
+//     mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
+//     // exclude JavaTemplate shims from license check
+//     exclude("src/main/resources/META-INF/rewrite/*.java")
+//     strictCheck = true
+// }
 
 configure<LicenseReportExtension> {
     renderers = arrayOf(com.github.jk1.license.render.CsvReportRenderer())
@@ -176,25 +191,6 @@ configure<PublishingExtension> {
                 }
             }
         }
-    }
-}
-
-configure<SpringBintrayExtension> {
-    org = "openrewrite"
-    repo = "maven"
-}
-
-project.withConvention(ArtifactoryPluginConvention::class) {
-    setContextUrl("https://oss.jfrog.org/artifactory")
-    publisherConfig.let {
-        val repository: PublisherConfig.Repository = it.javaClass
-                .getDeclaredField("repository")
-                .apply { isAccessible = true }
-                .get(it) as PublisherConfig.Repository
-
-        repository.setRepoKey("oss-snapshot-local")
-        repository.setUsername(project.findProperty("bintrayUser"))
-        repository.setPassword(project.findProperty("bintrayKey"))
     }
 }
 
