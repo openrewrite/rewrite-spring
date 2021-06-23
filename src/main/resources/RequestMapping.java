50407b0f41cf92022ff1d8631c2a028effb67534
Patrick <patway99@gmail.com>
2021-06-23 11:35:12 -0700

    Remove duplicate `GetMapping` definition from RequestMapping shim.  Fixes #79

--- src/main/resources/RequestMapping.java
+++ src/main/resources/RequestMapping.java
@@ -47,17 +47,6 @@ public @interface PutMapping {
 }
 ---
 package org.springframework.web.bind.annotation;
-public @interface GetMapping {
-    String name() default "";
-    String[] value() default {};
-    String[] path() default {};
-    String[] params() default {};
-    String[] headers() default {};
-    String[] consumes() default {};
-    String[] produces() default {};
-}
----
-package org.springframework.web.bind.annotation;
 public @interface PatchMapping {
     String name() default "";
     String[] value() default {};
