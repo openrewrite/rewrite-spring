437795df60fe3a12388a74882b4522cfe776da97
Jonathan Schneider <jkschneider@gmail.com>
2021-07-29 18:49:22 -0700

    SeparateApplicationYamlByProfile

---
+++ src/main/java/org/openrewrite/java/spring/SeparateApplicationYamlByProfile.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring;
+
+import org.openrewrite.Cursor;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.SourceFile;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.yaml.DeleteProperty;
+import org.openrewrite.yaml.JsonPathMatcher;
+import org.openrewrite.yaml.search.FindProperty;
+import org.openrewrite.yaml.tree.Yaml;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SeparateApplicationYamlByProfile extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Separate application YAML by profile";
+    }
+
+    @Override
+    public String getDescription() {
+        return "The Spring team's recommendation is to separate profile properties into their own YAML files now.";
+    }
+
+    @Override
+    protected List<SourceFile> visit(List<SourceFile> before, ExecutionContext ctx) {
+        JsonPathMatcher springProfile = new JsonPathMatcher("$.spring.config.activate.on-profile");
+
+        return ListUtils.flatMap(before, s -> {
+            if (s.getSourcePath().getFileSystem().getPathMatcher("glob:application.yml")
+                    .matches(s.getSourcePath().getFileName())) {
+                Yaml.Documents yaml = (Yaml.Documents) s;
+
+                Map<Yaml.Document, String> profiles = new HashMap<>(yaml.getDocuments().size());
+
+                //noinspection unchecked
+                Yaml.Documents mainYaml = yaml.withDocuments(ListUtils.map(
+                        (List<Yaml.Document>) yaml.getDocuments(),
+                        doc -> {
+                            String profileName = FindProperty.find(doc, "spring.config.activate.on-profile").stream()
+                                    .findAny()
+                                    .map(profile -> ((Yaml.Scalar) profile).getValue())
+                                    .orElse(null);
+
+                            if (profileName != null) {
+                                profiles.put((Yaml.Document) new DeleteProperty("spring.config.activate.on-profile", true, null)
+                                        .getVisitor().visit(doc, ctx, new Cursor(null, yaml)), profileName);
+                                return null;
+                            }
+
+                            return doc;
+                        }));
+
+                List<Yaml.Documents> profileYamls = profiles.entrySet().stream()
+                        .map(profile -> yaml
+                                .withDocuments(Collections.singletonList(profile.getKey().withExplicit(false)))
+                                .withSourcePath(yaml.getSourcePath().resolveSibling("application-" + profile.getValue() + ".yml"))
+                        )
+                        .collect(Collectors.toList());
+
+                return mainYaml.getDocuments().isEmpty() ? profileYamls : ListUtils.concat(mainYaml, profileYamls);
+            }
+
+            return s;
+        });
+    }
+}
---
+++ src/test/kotlin/org/openrewrite/java/spring/SeparateApplicationYamlByProfileTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.org.openrewrite.java.spring
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.openrewrite.Recipe
+import org.openrewrite.Result
+import org.openrewrite.java.spring.SeparateApplicationYamlByProfile
+import org.openrewrite.yaml.YamlParser
+import org.openrewrite.yaml.YamlRecipeTest
+import java.nio.file.Paths
+
+class SeparateApplicationYamlByProfileTest : YamlRecipeTest {
+    override val recipe: Recipe
+        get() = SeparateApplicationYamlByProfile()
+
+    @Test
+    fun separateProfile() {
+        val results = recipe.run(YamlParser().parse("""
+            name: main
+            ---
+            spring:
+                config:
+                    activate:
+                        on-profile: test
+            name: test
+        """.trimIndent()).map { it.withSourcePath(Paths.get("src/main/resources/application.yml")) }).groupByProfile()
+
+        assertThat(results["application.yml"]).isEqualTo("name: main")
+        assertThat(results["application-test.yml"]).isEqualTo("name: test")
+    }
+
+    @Test
+    fun onlyHasProfile() {
+        val results = recipe.run(YamlParser().parse("""
+            spring:
+                config:
+                    activate:
+                        on-profile: test
+            name: test
+        """.trimIndent()).map { it.withSourcePath(Paths.get("src/main/resources/application.yml")) }).groupByProfile()
+
+        assertThat(results["application.yml"]).isEqualTo(null)
+        assertThat(results["application-test.yml"]).isEqualTo("name: test")
+    }
+
+    private fun List<Result>.groupByProfile() = associate { r ->
+        (r.after ?: r.before).sourcePath.toFile().name to r.after?.printTrimmed()?.trim()
+    }
+}
