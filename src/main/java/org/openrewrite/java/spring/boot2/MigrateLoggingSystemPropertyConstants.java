cee6427dff96637e7eb8a830a3903c3a730d94ba
traceyyoshima <tracey.yoshima@gmail.com>
2021-08-10 15:16:25 -0700

    Added migration for deprecated constants in LoggingSystemProperties. Added recipe to upgrade 2.4.

---
+++ src/main/java/org/openrewrite/java/spring/boot2/MigrateLoggingSystemPropertyConstants.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.boot2;
+
+import org.openrewrite.*;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.java.AddImport;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.Flag;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.TypeUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
+public class MigrateLoggingSystemPropertyConstants extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Migrate to recommended constants in `LogbackLoggingSystemProperties` from deprecated values in `LoggingSystemProperties` ";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Replaces field and static access of deprecated fields in `LoggingSystemProperties` with the recommendations from `LogbackLoggingSystemProperties`. Deprecated in 2.4.x and removed in 2.6.0";
+    }
+
+    @Nullable
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
+        return new UsesType<>("org.springframework.boot.logging.logback.LoggingSystemProperties");
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new MigrateLoggingSystemPropertyConstants.UpdateDeprecatedConstantFieldNames();
+    }
+
+    private static class UpdateDeprecatedConstantFieldNames extends JavaIsoVisitor<ExecutionContext> {
+        private static final JavaType.FullyQualified ORIGINAL_FQN =
+                JavaType.Class.build("org.springframework.boot.logging.logback.LoggingSystemProperties");
+        private static final JavaType.FullyQualified NEW_NAME =
+                JavaType.Class.build("org.springframework.boot.logging.logback.LogbackLoggingSystemProperties");
+
+        private final Map<String, String> updateDeprecatedFields = new HashMap<>();
+
+        UpdateDeprecatedConstantFieldNames() {
+            updateDeprecatedFields.put("FILE_CLEAN_HISTORY_ON_START", "ROLLINGPOLICY_CLEAN_HISTORY_ON_START");
+            updateDeprecatedFields.put("FILE_MAX_HISTORY",            "ROLLINGPOLICY_MAX_HISTORY");
+            updateDeprecatedFields.put("FILE_MAX_SIZE",               "ROLLINGPOLICY_MAX_FILE_SIZE");
+            updateDeprecatedFields.put("FILE_TOTAL_SIZE_CAP",         "ROLLINGPOLICY_TOTAL_SIZE_CAP");
+            updateDeprecatedFields.put("ROLLING_FILE_NAME_PATTERN",   "ROLLINGPOLICY_FILE_NAME_PATTERN");
+        }
+
+        @Override
+        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+            J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
+
+            if (!classDecl.getSimpleName().equals(ORIGINAL_FQN.getClassName()) &&
+                    !classDecl.getSimpleName().equals(NEW_NAME.getClassName())) {
+                maybeRemoveImport(ORIGINAL_FQN);
+                maybeAddImport(NEW_NAME);
+            }
+
+            return cd;
+        }
+
+        @Override
+        public J.FieldAccess visitFieldAccess(J.FieldAccess fieldAccess, ExecutionContext ctx) {
+            J.FieldAccess fa = super.visitFieldAccess(fieldAccess, ctx);
+            if (isTargetClass() &&
+                    TypeUtils.isOfType(ORIGINAL_FQN, fa.getTarget().getType()) &&
+                    updateDeprecatedFields.containsKey(fa.getName().getSimpleName())) {
+
+                fa = fa.withName(fa.getName().withName(updateDeprecatedFields.get(fa.getName().getSimpleName())));
+                fa = fa.withTarget(J.Identifier.build(
+                        Tree.randomId(),
+                        fa.getTarget().getPrefix(),
+                        fa.getTarget().getMarkers(),
+                        NEW_NAME.getClassName(),
+                        NEW_NAME));
+            }
+            return fa;
+        }
+
+        @Override
+        public J.Identifier visitIdentifier(J.Identifier identifier, ExecutionContext ctx) {
+            J.Identifier id = super.visitIdentifier(identifier, ctx);
+            if (isTargetClass() && isTargetFieldType(id) && updateDeprecatedFields.containsKey(id.getSimpleName())) {
+                JavaType.Variable fieldType = ((JavaType.Variable)id.getFieldType());
+                id = J.Identifier.build(
+                        Tree.randomId(),
+                        id.getPrefix(),
+                        id.getMarkers(),
+                        updateDeprecatedFields.get(id.getSimpleName()),
+                        id.getType(),
+                        JavaType.Variable.build(
+                                updateDeprecatedFields.get(id.getSimpleName()),
+                                NEW_NAME,
+                                fieldType == null ? 0 : Flag.flagsToBitMap(fieldType.getFlags())));
+
+                doAfterVisit(new AddImport<>(NEW_NAME.getFullyQualifiedName(), id.getSimpleName(), false));
+            }
+            return id;
+        }
+
+        private boolean isTargetClass() {
+            Cursor parentCursor = getCursor().dropParentUntil(
+                    is -> is instanceof J.CompilationUnit ||
+                          is instanceof J.ClassDeclaration);
+            return (parentCursor.getValue() instanceof J.ClassDeclaration &&
+                    !((J.ClassDeclaration) parentCursor.getValue()).getName().getSimpleName().equals(ORIGINAL_FQN.getClassName()) &&
+                    !((J.ClassDeclaration) parentCursor.getValue()).getName().getSimpleName().equals(NEW_NAME.getClassName()));
+        }
+
+        private boolean isTargetFieldType(J.Identifier identifier) {
+            if (identifier.getFieldType() != null && identifier.getFieldType() instanceof JavaType.Variable) {
+                JavaType.Variable fieldType = ((JavaType.Variable)identifier.getFieldType());
+                return Objects.equals(ORIGINAL_FQN, fieldType.getType());
+            }
+            return false;
+        }
+    }
+
+}
---
+++ src/test/kotlin/org/openrewrite/java/spring/boot2/MigrateLoggingSystemPropertyConstantsTest.kt
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.org.openrewrite.java.spring.boot2
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.java.JavaParser
+import org.openrewrite.java.JavaRecipeTest
+import org.openrewrite.java.spring.boot2.MigrateLoggingSystemPropertyConstants
+
+class MigrateLoggingSystemPropertyConstantsTest : JavaRecipeTest {
+    override val parser: JavaParser
+        get() = JavaParser.fromJavaVersion().classpath("java").build()
+
+    override val recipe = MigrateLoggingSystemPropertyConstants()
+
+    // Explicit pseudo files, since the files exist in spring 2.4.x and 1.4.x is loaded on the classpath.
+    companion object {
+        const val source = """
+            package org.springframework.boot.logging.logback;
+
+            public class LoggingSystemProperties {
+                public static final String CONSOLE_LOG_CHARSET = "";
+                public static final String CONSOLE_LOG_PATTERN = "";
+                public static final String FILE_CLEAN_HISTORY_ON_START = "";
+                public static final String FILE_MAX_HISTORY = "";
+                public static final String FILE_MAX_SIZE = "";
+                public static final String FILE_TOTAL_SIZE_CAP = "";
+                public static final String ROLLING_FILE_NAME_PATTERN = "";
+            }
+        """
+
+        const val target = """
+            package org.springframework.boot.logging.logback;
+
+            public class LogbackLoggingSystemProperties {
+                public static final String ROLLINGPOLICY_CLEAN_HISTORY_ON_START = "";
+                public static final String ROLLINGPOLICY_MAX_HISTORY = "";
+                public static final String ROLLINGPOLICY_MAX_FILE_SIZE = "";
+                public static final String ROLLINGPOLICY_TOTAL_SIZE_CAP = "";
+                public static final String ROLLINGPOLICY_FILE_NAME_PATTERN = "";
+             
+        """
+    }
+
+    @Test
+    fun doNotUpdateFieldsInTargetClass() = assertUnchanged(
+        before = source
+    )
+
+    @Test
+    fun doNotUpdateFieldsInNewClass() = assertUnchanged(
+        before = target
+    )
+
+    @Test
+    fun doNotUpdateCurrentAPIs() = assertUnchanged(
+        dependsOn = arrayOf(source),
+        before = """
+            package org.test;
+
+            import org.springframework.boot.logging.logback.LoggingSystemProperties;
+
+            import static org.springframework.boot.logging.logback.LoggingSystemProperties.CONSOLE_LOG_PATTERN;
+            
+            class Test {
+                void methodA() {
+                    String doNotUpdate = LoggingSystemProperties.CONSOLE_LOG_CHARSET;
+                }
+                void methodB() {
+                    String doNotUpdate = CONSOLE_LOG_PATTERN;
+                }
+            }
+        """
+    )
+
+    @Test
+    fun updateToRecommendedReplacements() = assertChanged(
+        dependsOn = arrayOf(source, target),
+        before = """
+            package org.test;
+
+            import org.springframework.boot.logging.logback.LoggingSystemProperties;
+
+            import static org.springframework.boot.logging.logback.LoggingSystemProperties.FILE_MAX_SIZE;
+            import static org.springframework.boot.logging.logback.LoggingSystemProperties.FILE_TOTAL_SIZE_CAP;
+            import static org.springframework.boot.logging.logback.LoggingSystemProperties.ROLLING_FILE_NAME_PATTERN;
+            
+            class Test {
+                void methodA() {
+                    String valueA = LoggingSystemProperties.FILE_CLEAN_HISTORY_ON_START;
+                    String valueB = LoggingSystemProperties.FILE_MAX_HISTORY;
+                }
+                void methodB() {
+                    String valueA = FILE_MAX_SIZE;
+                    String valueB = FILE_TOTAL_SIZE_CAP;
+                    String valueC = ROLLING_FILE_NAME_PATTERN;
+                }
+            }
+        """,
+        after = """
+            package org.test;
+
+            import org.springframework.boot.logging.logback.LogbackLoggingSystemProperties;
+
+            import static org.springframework.boot.logging.logback.LogbackLoggingSystemProperties.*;
+            
+            class Test {
+                void methodA() {
+                    String valueA = LogbackLoggingSystemProperties.ROLLINGPOLICY_CLEAN_HISTORY_ON_START;
+                    String valueB = LogbackLoggingSystemProperties.ROLLINGPOLICY_MAX_HISTORY;
+                }
+                void methodB() {
+                    String valueA = ROLLINGPOLICY_MAX_FILE_SIZE;
+                    String valueB = ROLLINGPOLICY_TOTAL_SIZE_CAP;
+                    String valueC = ROLLINGPOLICY_FILE_NAME_PATTERN;
+                }
+            }
+        """
+    )
+}
