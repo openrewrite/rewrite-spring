3fb665a8f7381f990d9ff22f4c2b45485fb443eb
Jonathan Schneider <jkschneider@gmail.com>
2021-08-20 11:04:30 -0700

    Verify that Spring Boot upgrade chains successively upgrade versions. (fixes #92)

---
+++ src/test/kotlin/org/openrewrite/java/spring/boot2/UpgradeDependenciesTest.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.boot2
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.Issue
+import org.openrewrite.Recipe
+import org.openrewrite.config.Environment
+import org.openrewrite.maven.MavenRecipeTest
+
+class UpgradeDependenciesTest : MavenRecipeTest {
+
+    override val recipe: Recipe
+        get() = Environment.builder()
+            .scanRuntimeClasspath()
+            .build()
+            .activateRecipes("org.openrewrite.java.spring.boot2.UpgradeSpringBoot_2_4")
+
+    @Issue("https://github.com/openrewrite/rewrite-spring/issues/92")
+    @Test
+    fun upgradeVersion() = assertChanged(
+        before = """
+            <project>
+              <modelVersion>4.0.0</modelVersion>
+              
+              <groupId>com.mycompany.app</groupId>
+              <artifactId>my-app</artifactId>
+              <version>1</version>
+              
+              <dependencies>
+                <dependency>
+                  <groupId>org.springframework.boot</groupId>
+                  <artifactId>spring-boot</artifactId>
+                  <version>1.5.1.RELEASE</version>
+                </dependency>
+              </dependencies>
+            </project>
+        """,
+        after = """
+            <project>
+              <modelVersion>4.0.0</modelVersion>
+              
+              <groupId>com.mycompany.app</groupId>
+              <artifactId>my-app</artifactId>
+              <version>1</version>
+              
+              <dependencies>
+                <dependency>
+                  <groupId>org.springframework.boot</groupId>
+                  <artifactId>spring-boot</artifactId>
+                  <version>2.4.10</version>
+                </dependency>
+              </dependencies>
+            </project>
+        """
+    )
+}
