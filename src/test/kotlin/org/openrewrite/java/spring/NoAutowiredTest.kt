a437efcc32d6bd686b439231d9ac6ebf88ce7e36
Aaron Gershman <aegershman@gmail.com>
2021-08-23 15:26:16 -0500

    polish, Add deprecation suppression for Required annotation in NoAutowiredTest

--- src/test/kotlin/org/openrewrite/java/spring/NoAutowiredTest.kt
+++ src/test/kotlin/org/openrewrite/java/spring/NoAutowiredTest.kt
@@ -21,6 +21,7 @@ import org.openrewrite.Recipe
 import org.openrewrite.java.JavaParser
 import org.openrewrite.java.JavaRecipeTest
 
+@Suppress("deprecation", "MissortedModifiers")
 class NoAutowiredTest : JavaRecipeTest {
     override val parser: JavaParser
         get() = JavaParser.fromJavaVersion()
