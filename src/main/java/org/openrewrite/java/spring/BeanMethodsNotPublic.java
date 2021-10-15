5034ee615fe90f7bf6d72287ca17c25822166412
Sam Snyder <sam@moderne.io>
2021-10-14 17:27:20 -0700

    Moved method override checking to TypeUtils

--- src/main/java/org/openrewrite/java/spring/BeanMethodsNotPublic.java
+++ src/main/java/org/openrewrite/java/spring/BeanMethodsNotPublic.java
@@ -19,13 +19,13 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.internal.lang.Nullable;
-import org.openrewrite.java.*;
+import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.ChangeMethodAccessLevelVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.J;
-import org.openrewrite.java.tree.JavaType;
 import org.openrewrite.java.tree.TypeUtils;
-
-import java.util.List;
 
 public class BeanMethodsNotPublic extends Recipe {
     @Override
@@ -56,7 +56,8 @@ public class BeanMethodsNotPublic extends Recipe {
         public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext executionContext) {
             J.MethodDeclaration m = super.visitMethodDeclaration(method, executionContext);
 
-            if (m.getAllAnnotations().stream().anyMatch(BEAN_ANNOTATION_MATCHER::matches) && !methodIsAnOverride(method)) {
+            if (m.getAllAnnotations().stream().anyMatch(BEAN_ANNOTATION_MATCHER::matches)
+                    && Boolean.FALSE.equals(TypeUtils.isAnOverride(method.getType()))) {
                 // remove public modifier and copy any associated comments to the method
                 doAfterVisit(new ChangeMethodAccessLevelVisitor<>(new MethodMatcher(method), null));
             }
@@ -64,56 +65,5 @@ public class BeanMethodsNotPublic extends Recipe {
             return m;
         }
 
-        private boolean methodIsAnOverride(J.MethodDeclaration m) {
-            if(m.getType() == null || m.getType().getGenericSignature() == null) {
-                // When missing type information, be conservative and make no changes
-                return true;
-            }
-            JavaType.FullyQualified dt = m.getType().getDeclaringType();
-            List<JavaType> argTypes = m.getType().getGenericSignature().getParamTypes();
-            return declaresMethod(dt.getSupertype(), m.getSimpleName(), argTypes)
-                    || dt.getInterfaces().stream().anyMatch(i -> declaresMethod(i, m.getSimpleName(), argTypes));
-        }
-
-        private boolean methodHasSignature(JavaType.Method m, String name, List<JavaType> argTypes) {
-            if(!name.equals(m.getName())) {
-                return false;
-            }
-            if(m.getGenericSignature() == null) {
-                return false;
-            }
-            List<JavaType> mArgs = m.getGenericSignature().getParamTypes();
-            if(mArgs.size() != argTypes.size()) {
-                return false;
-            }
-            for(int i = 0; i < mArgs.size(); i++) {
-                if(!TypeUtils.isOfType(mArgs.get(i), argTypes.get(i))) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private boolean declaresMethod(@Nullable JavaType.FullyQualified clazz, String name, List<JavaType> argTypes) {
-            if(clazz == null) {
-                return false;
-            }
-            if(clazz.getMethods().stream().anyMatch(m -> methodHasSignature(m, name, argTypes))) {
-                return true;
-            }
-            JavaType.FullyQualified supertype = clazz.getSupertype();
-            if(declaresMethod(supertype, name, argTypes)) {
-                return true;
-            }
-
-            for(JavaType.FullyQualified i : clazz.getInterfaces()) {
-                if(declaresMethod(i, name, argTypes)) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
     }
 }
