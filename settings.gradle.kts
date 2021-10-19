2c4f3fd92fd7e47818cf8625135dc98b6425d538
Aaron Gershman <aegershman@gmail.com>
2021-10-19 14:29:57 -0500

    Initial gradle enterprise setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,25 @@
 rootProject.name = "rewrite-spring"
+
+plugins {
+    id("com.gradle.enterprise") version "3.7"
+}
+
+gradleEnterprise {
+    server = "https://ge.openrewrite.org/"
+
+    buildCache {
+        local {
+            isEnabled = true
+        }
+
+        remote(HttpBuildCache::class) {
+            isPush = true
+            url = uri("https://ge.openrewrite.org/cache/")
+        }
+
+    }
+
+    buildScan {
+        publishAlways()
+    }
+}
