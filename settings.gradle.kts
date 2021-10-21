9289aecd3ea90e8ed6c55e64a4c4e50cfcdecbf9
Aaron Gershman <aegershman@gmail.com>
2021-10-21 14:37:37 -0500

    Update gradle-enterprise configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,24 +2,27 @@ rootProject.name = "rewrite-spring"
 
 plugins {
     id("com.gradle.enterprise") version "3.7"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.4.2"
 }
 
 gradleEnterprise {
+    val isCiServer = System.getenv("CI")?.equals("true") ?: false
     server = "https://ge.openrewrite.org/"
 
     buildCache {
-        local {
-            isEnabled = true
-        }
-
         remote(HttpBuildCache::class) {
-            isPush = true
             url = uri("https://ge.openrewrite.org/cache/")
+            isPush = isCiServer
         }
-
     }
 
     buildScan {
         publishAlways()
+        isUploadInBackground = !isCiServer
+
+        capture {
+            isTaskInputFiles = true
+        }
     }
+
 }
