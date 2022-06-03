5e734571a04cec41cacfdece398019f138f95f58
BoykoAlex <aboyko@pivotal.io>
2022-06-03 16:24:28 -0400

    Boot 3: Remove @ConstructorBinding annotation on @ConfigurationProperties type

---
+++ src/testWithSpringBoot_2_4/kotlin/org/openrewrite/java/spring/boot3/RemoveConstructorBindingAnnotationTest.kt
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2022 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.boot3
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.Recipe
+import org.openrewrite.java.JavaParser
+import org.openrewrite.java.JavaRecipeTest
+
+/**
+ * @author Alex Boyko
+ */
+class RemoveConstructorBindingAnnotationTest : JavaRecipeTest {
+
+    override val parser: JavaParser
+        get() = JavaParser.fromJavaVersion()
+            .logCompilationWarningsAndErrors(true)
+            .classpath("spring-boot")
+            .build()
+
+    override val recipe: Recipe
+        get() = RemoveConstructorBindingAnnotation()
+
+
+    @Test
+    fun topLevelTypeAnnotation() = assertChanged(
+        before = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            import org.springframework.boot.context.properties.ConstructorBinding;
+            
+            @ConfigurationProperties
+            @ConstructorBinding
+            class A {
+                void method() {
+                }
+            }
+        """,
+        after = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            
+            @ConfigurationProperties
+            class A {
+                void method() {
+                }
+            }
+        """
+    )
+
+    @Test
+    fun constructorAnnotation() = assertUnchanged(
+        before = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            import org.springframework.boot.context.properties.ConstructorBinding;
+            
+            @ConfigurationProperties
+            class A {
+                @ConstructorBinding
+                A() {
+                }
+            }
+        """
+    )
+
+    @Test
+    fun topLevelTypeAnnotationWithoutConfigProperties() = assertUnchanged(
+        before = """
+            import org.springframework.boot.context.properties.ConstructorBinding;
+            
+            @ConstructorBinding
+            class A {
+            }
+        """
+    )
+
+    @Test
+    fun constructorAnnotationWithMultipleConstructors() = assertUnchanged(
+        before = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            import org.springframework.boot.context.properties.ConstructorBinding;
+            
+            @ConfigurationProperties
+            class A {
+                A() {
+                }
+                @ConstructorBinding
+                A(int n) {
+                }
+            }
+        """
+    )
+
+    @Test
+    fun noConstrBindingAnnotation() = assertUnchanged(
+        before = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            
+            @ConfigurationProperties
+            class A {
+                A() {
+                }
+                A(int n) {
+                }
+            }
+        """
+    )
+
+    @Test
+    fun topLevelTypeAnnotationInnerClass() = assertChanged(
+        before = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            import org.springframework.boot.context.properties.ConstructorBinding;
+            
+            class A {
+                void method() {
+                }
+                
+                @ConfigurationProperties
+                @ConstructorBinding
+                static class B {
+                }
+            }
+        """,
+        after = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            
+            class A {
+                void method() {
+                }
+                
+                @ConfigurationProperties
+                static class B {
+                }
+            }
+        """
+    )
+
+    @Test
+    fun constructorAnnotationInnerClass() = assertChanged(
+        before = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            import org.springframework.boot.context.properties.ConstructorBinding;
+            
+            @ConfigurationProperties
+            @ConstructorBinding
+            class A {
+                void method() {
+                }
+                
+                @ConfigurationProperties
+                static class B {
+                    @ConstructorBinding
+                    B() {
+                    }
+                }
+            }
+        """,
+        after = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            import org.springframework.boot.context.properties.ConstructorBinding;
+            
+            @ConfigurationProperties
+            class A {
+                void method() {
+                }
+                
+                @ConfigurationProperties
+                static class B {
+                    @ConstructorBinding
+                    B() {
+                    }
+                }
+            }
+        """
+    )
+
+    @Test
+    fun constructorAndTypeAnnotationWithMultipleConstructorsInnerClass() = assertUnchanged(
+        before = """
+            import org.springframework.boot.context.properties.ConfigurationProperties;
+            import org.springframework.boot.context.properties.ConstructorBinding;
+            
+            class A {
+                @ConfigurationProperties
+                static class B {
+                    B() {
+                    }
+                    @ConstructorBinding
+                    B(int n) {
+                    }
+                }
+            }
+        """
+    )
+
+}
