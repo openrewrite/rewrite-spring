feb03fd8dee319df06ab77fb1d7bc1f418ad6e98
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-07-01 15:29:20 +0200

    Quick fix for test, first iteration

--- src/main/java/org/openrewrite/java/spring/boot2/SAMLRelyingPartyPropertyApplicationPropertiesMove.java
+++ src/main/java/org/openrewrite/java/spring/boot2/SAMLRelyingPartyPropertyApplicationPropertiesMove.java
@@ -1,6 +1,13 @@
 package org.openrewrite.java.spring.boot2;
 
+import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.properties.PropertiesVisitor;
+import org.openrewrite.properties.tree.Properties;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class SAMLRelyingPartyPropertyApplicationPropertiesMove extends Recipe {
     @Override
@@ -13,5 +20,21 @@ public class SAMLRelyingPartyPropertyApplicationPropertiesMove extends Recipe {
         return "Renames spring.security.saml2.relyingparty.registration.(any).identityprovider to " +
                 "spring.security.saml2.relyingparty.registration.(any).assertingparty";
     }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+
+        return new PropertiesVisitor<ExecutionContext>() {
+
+
+            @Override
+            public Properties visitEntry(Properties.Entry entry, ExecutionContext executionContext) {
+                String updatedKey = entry.getKey().replaceAll("identityprovider", "assertingparty");
+                Properties.Entry updatedEntry = new Properties.Entry(UUID.randomUUID(), entry.getPrefix(), entry.getMarkers(), updatedKey, entry.getBeforeEquals(), entry.getValue());
+                return super.visitEntry(updatedEntry, executionContext);
+            }
+        };
+    }
+
 
 }
