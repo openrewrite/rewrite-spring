3a511027f79a655ed7c9a32c23c229fc1a727884
Jonathan Schneider <jkschneider@gmail.com>
2022-12-13 16:44:26 -0500

    Use org.openrewrite.build.recipe-library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,12 @@
-import com.github.jk1.license.LicenseReportExtension
-import nebula.plugin.contacts.Contact
-import nebula.plugin.contacts.ContactsExtension
-import nl.javadude.gradle.plugins.license.LicenseExtension
 import java.util.*
 
-buildscript {
-    repositories {
-        gradlePluginPortal()
-    }
-}
-
 plugins {
-    `java-library`
-    `maven-publish`
-    signing
-
-    id("nebula.maven-resolved-dependencies") version "17.3.2"
-    id("nebula.release") version "15.3.1"
-    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
-
-    id("com.github.hierynomus.license") version "0.16.1"
-    id("com.github.jk1.dependency-license-report") version "1.16"
-    id("org.owasp.dependencycheck") version "latest.release"
-
-    id("nebula.maven-publish") version "17.3.2"
-    id("nebula.contacts") version "5.1.0"
-    id("nebula.info") version "11.1.0"
-
-    id("nebula.javadoc-jar") version "17.3.2"
-    id("nebula.source-jar") version "17.3.2"
-    id("nebula.maven-apache-license") version "17.3.2"
-
+    id("org.openrewrite.build.recipe-library") version "latest.release"
     id("org.openrewrite.rewrite") version "latest.release"
 }
 
-apply(plugin = "nebula.publish-verification")
-
 rewrite {
     activeRecipe("org.openrewrite.java.format.AutoFormat", "org.openrewrite.java.cleanup.Cleanup")
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
-    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-dependencyCheck {
-    analyzers.nodeAuditEnabled = false
-    analyzers.nodeEnabled = false
-    analyzers.assemblyEnabled = false
-    failBuildOnCVSS = 9.0F
-    suppressionFile = "suppressions.xml"
-    format = org.owasp.dependencycheck.reporting.ReportGenerator.Format.valueOf(project.properties["dependencyCheckFormat"] as String? ?: "HTML")
 }
 
 group = "org.openrewrite.recipe"
@@ -76,35 +26,9 @@ sourceSets {
 }
 
 repositories {
-    if(!project.hasProperty("releasing")) {
-        mavenLocal()
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-        }
-    }
-    mavenCentral()
     maven {
         url = uri("https://repo.spring.io/milestone")
     }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype()
-    }
-}
-
-val signingKey: String? by project
-val signingPassword: String? by project
-val requireSigning = project.hasProperty("forceSigning") || project.hasProperty("releasing")
-if(signingKey != null && signingPassword != null) {
-    signing {
-        isRequired = requireSigning
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(publishing.publications["nebula"])
-    }
-} else if(requireSigning) {
-    throw RuntimeException("Artifact signing is required, but signingKey and/or signingPassword are null")
 }
 
 configurations {
@@ -118,26 +42,11 @@ configurations {
             extendsFrom(getByName("testImplementation"))
         }
     }
-
-    all {
-        resolutionStrategy {
-            cacheChangingModulesFor(0, TimeUnit.SECONDS)
-            cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
-        }
-    }
 }
 
-var rewriteVersion = if(project.hasProperty("releasing")) {
-    "latest.release"
-} else {
-    "latest.integration"
-}
-
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 var springBoot3Version = "3.0.0-RC1"
 dependencies {
-    compileOnly("org.projectlombok:lombok:latest.release")
-    annotationProcessor("org.projectlombok:lombok:latest.release")
-
     implementation("org.openrewrite:rewrite-java:${rewriteVersion}")
     implementation("org.openrewrite:rewrite-xml:${rewriteVersion}")
     implementation("org.openrewrite:rewrite-properties:${rewriteVersion}")
@@ -149,23 +58,13 @@ dependencies {
     runtimeOnly("org.openrewrite.recipe:rewrite-migrate-java:${rewriteVersion}")
     runtimeOnly("org.openrewrite:rewrite-java-17:$rewriteVersion")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:latest.release")
-
-    testImplementation("org.openrewrite:rewrite-test:${rewriteVersion}")
-    testImplementation("org.openrewrite:rewrite-java-tck:${rewriteVersion}")
-
-    testImplementation("org.assertj:assertj-core:latest.release")
     testImplementation("com.github.marschall:memoryfilesystem:latest.release")
 
     // for generating properties migration configurations
-    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.13.4")
     testImplementation("io.github.classgraph:classgraph:latest.release")
     testImplementation("org.openrewrite:rewrite-java-17:${rewriteVersion}")
     testImplementation("org.openrewrite.recipe:rewrite-migrate-java:${rewriteVersion}")
     testImplementation("org.openrewrite.recipe:rewrite-testing-frameworks:${rewriteVersion}")
-
 
     "testWithSpringBoot_1_5RuntimeOnly"("org.springframework:spring-web:4.+")
     "testWithSpringBoot_1_5RuntimeOnly"("org.springframework.boot:spring-boot:1.5.+")
@@ -223,11 +122,6 @@ dependencies {
 
 }
 
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-    jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
-}
-
 springBootVersions.forEach { version ->
     val sourceSetName = "testWithSpringBoot_${version}"
     val sourceSetReference = project.sourceSets.getByName(sourceSetName)
@@ -242,64 +136,5 @@ springBootVersions.forEach { version ->
     }
     tasks.test {
         dependsOn(testTask)
-    }
-}
-
-tasks.named<JavaCompile>("compileJava") {
-    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-    targetCompatibility = JavaVersion.VERSION_1_8.toString()
-
-    options.isFork = true
-    options.compilerArgs.addAll(listOf("--release", "8"))
-    options.encoding = "UTF-8"
-    options.compilerArgs.add("-parameters")
-}
-
-configure<ContactsExtension> {
-    val j = Contact("jkschneider@gmail.com")
-    j.moniker("Jonathan Schneider")
-
-    people["jkschneider@gmail.com"] = j
-}
-
- configure<LicenseExtension> {
-     ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
-     skipExistingHeaders = true
-     header = project.rootProject.file("gradle/licenseHeader.txt")
-     mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
-     // exclude JavaTemplate shims from license check
-     exclude("src/main/resources/META-INF/rewrite/*.java")
-     strictCheck = true
- }
-
-configure<LicenseReportExtension> {
-    renderers = arrayOf(com.github.jk1.license.render.CsvReportRenderer())
-}
-
-configure<PublishingExtension> {
-    publications {
-        named("nebula", MavenPublication::class.java) {
-            suppressPomMetadataWarningsFor("runtimeElements")
-
-            pom.withXml {
-                (asElement().getElementsByTagName("dependencies").item(0) as org.w3c.dom.Element).let { dependencies ->
-                    dependencies.getElementsByTagName("dependency").let { dependencyList ->
-                        var i = 0
-                        var length = dependencyList.length
-                        while (i < length) {
-                            (dependencyList.item(i) as org.w3c.dom.Element).let { dependency ->
-                                if ((dependency.getElementsByTagName("scope")
-                                        .item(0) as org.w3c.dom.Element).textContent == "provided") {
-                                    dependencies.removeChild(dependency)
-                                    i--
-                                    length--
-                                }
-                            }
-                            i++
-                        }
-                    }
-                }
-            }
-        }
     }
 }
