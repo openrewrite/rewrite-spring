785882a4be953e0892dbb0e93294ecdd2202d07d
Tim te Beek <tim@moderne.io>
2023-01-23 14:39:40 +0100

    Add test to verify mysql-driver updates

---
+++ src/testWithSpringBoot_2_7/java/org/openrewrite/java/spring/boot2/UpdateMysqlDriverArtifactIdTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.boot2;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.config.Environment;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.maven.Assertions.pomXml;
+
+public class UpdateMysqlDriverArtifactIdTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+                .recipe(Environment.builder()
+                        .scanRuntimeClasspath("org.openrewrite.java.spring.boot2")
+                        .build()
+                        .activateRecipes("org.openrewrite.java.spring.boot2.UpgradeSpringBoot_2_7")
+                );
+    }
+
+    @Test
+    void switchArtifactIdAndUpdateVersionNumber() {
+        rewriteRun(
+                //language=xml
+                pomXml(
+                        """
+                        <project>
+                          <modelVersion>4.0.0</modelVersion>
+                          <groupId>com.example</groupId>
+                          <artifactId>demo</artifactId>
+                          <version>0.0.1-SNAPSHOT</version>
+                          <dependencies>
+                            <dependency>
+                              <groupId>com.mysql</groupId>
+                              <artifactId>mysql-connector-java</artifactId>
+                              <version>8.0.30</version>
+                              <scope>runtime</scope>
+                            </dependency>
+                          </dependencies>
+                        </project>
+                        """,
+                        """
+                        <project>
+                          <modelVersion>4.0.0</modelVersion>
+                          <groupId>com.example</groupId>
+                          <artifactId>demo</artifactId>
+                          <version>0.0.1-SNAPSHOT</version>
+                          <dependencies>
+                            <dependency>
+                              <groupId>com.mysql</groupId>
+                              <artifactId>mysql-connector-j</artifactId>
+                              <version>8.0.32</version>
+                              <scope>runtime</scope>
+                            </dependency>
+                          </dependencies>
+                        </project>
+                        """
+                )
+        );
+    }
+    @Test
+    void doNotPinWhenNotVersioned() {
+        rewriteRun(
+                //language=xml
+                pomXml(
+                        """
+                        <project>
+                          <modelVersion>4.0.0</modelVersion>
+                          <groupId>com.example</groupId>
+                          <artifactId>demo</artifactId>
+                          <version>0.0.1-SNAPSHOT</version>
+                          <parent>
+                            <groupId>org.springframework.boot</groupId>
+                            <artifactId>spring-boot-starter-parent</artifactId>
+                            <version>2.7.8</version>
+                            <relativePath/> <!-- lookup parent from repository -->
+                          </parent>
+                          <dependencies>
+                            <dependency>
+                               <groupId>com.mysql</groupId>
+                               <artifactId>mysql-connector-java</artifactId>
+                               <scope>runtime</scope>
+                            </dependency>
+                          </dependencies>
+                        </project>
+                        """,
+                        """
+                        <project>
+                          <modelVersion>4.0.0</modelVersion>
+                          <groupId>com.example</groupId>
+                          <artifactId>demo</artifactId>
+                          <version>0.0.1-SNAPSHOT</version>
+                          <parent>
+                              <groupId>org.springframework.boot</groupId>
+                              <artifactId>spring-boot-starter-parent</artifactId>
+                              <version>2.7.8</version>
+                              <relativePath/> <!-- lookup parent from repository -->
+                          </parent>
+                          <dependencies>
+                            <dependency>
+                               <groupId>com.mysql</groupId>
+                               <artifactId>mysql-connector-j</artifactId>
+                               <scope>runtime</scope>
+                            </dependency>
+                          </dependencies>
+                        </project>
+                        """
+                )
+        );
+    }
+
+}
