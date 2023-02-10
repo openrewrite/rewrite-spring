9a71547bb7e38cf8825a2650ea056aaa5caafee7
Pascal Desprez <pascal.desprez@fr.ibm.com>
2023-02-10 15:03:06 +0100

    MigrateStepBuilderFactory recipe draft

---
+++ src/main/java/org/openrewrite/java/spring/batch/MigrateStepBuilderFactory.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.batch;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.J.ClassDeclaration;
+import org.openrewrite.java.tree.J.MethodDeclaration;
+import org.openrewrite.java.tree.Statement;
+import org.openrewrite.java.tree.TypeUtils;
+
+public class MigrateStepBuilderFactory extends Recipe {
+
+    private static final MethodMatcher STEP_BUILDER_FACTORY = new MethodMatcher(
+            "org.springframework.batch.core.configuration.annotation.StepBuilderFactory get(java.lang.String)");
+
+    @Override
+    public String getDisplayName() {
+        return "Migrate `StepBuilderFactory` to `StepBuilder`";
+    }
+
+    @Override
+    public String getDescription() {
+        return "`StepBuilderFactory` was deprecated in spring-batch 5.x. It is replaced by `StepBuilder`.";
+    }
+
+    @Override
+    public Duration getEstimatedEffortPerOccurrence() {
+        return Duration.ofMinutes(5);
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getApplicableTest() {
+        return new UsesMethod<>(STEP_BUILDER_FACTORY);
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaVisitor<ExecutionContext>() {
+
+            @Override
+            public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                if (STEP_BUILDER_FACTORY.matches(method)) {
+
+                    J.ClassDeclaration clazz = getCursor().firstEnclosingOrThrow(J.ClassDeclaration.class);
+                    J.MethodDeclaration enclosingMethod = getCursor().firstEnclosingOrThrow(J.MethodDeclaration.class);
+
+                    maybeAddImport("org.springframework.batch.core.step.builder.StepBuilder", false);
+                    maybeRemoveImport("org.springframework.batch.core.configuration.annotation.StepBuilderFactory");
+                    maybeAddImport("org.springframework.batch.core.repository.JobRepository");
+
+                    doAfterVisit(new MigrateStepBuilderFactory.RemoveStepBuilderFactoryVisitor(clazz, enclosingMethod));
+
+                    return (method.withTemplate(
+                            JavaTemplate
+                                    .builder(() -> getCursor().getParentTreeCursor(),
+                                            "new StepBuilder(#{any(java.lang.String)}, jobRepository)")
+                                    .javaParser(() -> JavaParser.fromJavaVersion()
+                                            .classpathFromResources(ctx, "spring-batch-core-5.0.0",
+                                                    "spring-batch-infrastructure-5.0.0")
+                                            .build())
+                                    .imports("org.springframework.batch.core.repository.JobRepository",
+                                            "org.springframework.batch.core.step.builder.StepBuilder")
+                                    .build(),
+                            method.getCoordinates().replace(), method.getArguments().get(0)));
+                }
+                return super.visitMethodInvocation(method, ctx);
+            }
+        };
+    }
+
+    private static class RemoveStepBuilderFactoryVisitor extends JavaIsoVisitor<ExecutionContext> {
+
+        private final ClassDeclaration scope;
+
+        private final MethodDeclaration enclosingMethod;
+
+        public RemoveStepBuilderFactoryVisitor(ClassDeclaration scope, MethodDeclaration enclosingMethod) {
+            this.scope = scope;
+            this.enclosingMethod = enclosingMethod;
+        }
+
+        @Override
+        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+            J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
+
+            if (!cd.equals(scope)) {
+                return cd;
+            }
+            cd = cd.withBody(cd.getBody().withStatements(ListUtils.map(cd.getBody().getStatements(), statement -> {
+                if (statement instanceof J.VariableDeclarations
+                        && ((J.VariableDeclarations) statement).getTypeExpression() != null) {
+                    if (TypeUtils.isOfClassType(((J.VariableDeclarations) statement).getTypeExpression().getType(),
+                            "org.springframework.batch.core.configuration.annotation.StepBuilderFactory")) {
+                        return null;
+                    }
+                }
+                return statement;
+            })));
+            maybeRemoveImport("org.springframework.batch.core.configuration.annotation.StepBuilderFactory");
+            return cd;
+        }
+
+        @Override
+        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration methodDecl, ExecutionContext ctx) {
+            J.MethodDeclaration md = super.visitMethodDeclaration(methodDecl, ctx);
+
+            if (!enclosingMethod.equals(md) && !md.isConstructor()) {
+                return md;
+            }
+
+            List<Object> params = md.getParameters().stream()
+                    .filter(j -> !(j instanceof J.Empty) && !isJobBuilderFactoryParameter(j))
+                    .collect(Collectors.toList());
+
+            if (params.isEmpty() && md.isConstructor()) {
+                //noinspection DataFlowIssue
+                return null;
+            }
+
+            if (md.getParameters().stream().noneMatch(this::isJobRepositoryParameter) && !md.isConstructor()) {
+                params.add("JobRepository jobRepository");
+            }
+
+            JavaTemplate paramsTemplate = JavaTemplate
+                    .builder(this::getCursor, params.stream().map(p -> "#{}").collect(Collectors.joining(", ")))
+                    .imports("org.springframework.batch.core.repository.JobRepository",
+                            "org.springframework.batch.core.step.builder.StepBuilder",
+                            "org.springframework.batch.core.Step")
+                    .javaParser(
+                            () -> JavaParser.fromJavaVersion()
+                                    .classpathFromResources(ctx, "spring-batch-core-5.0.0",
+                                            "spring-batch-infrastructure-5.0.0")
+                                    .build())
+                    //.doBeforeParseTemplate(System.out::println)
+                    .build();
+
+            md = md.withTemplate(paramsTemplate, md.getCoordinates().replaceParameters(), params.toArray());
+
+            maybeRemoveImport("org.springframework.batch.core.configuration.annotation.StepBuilderFactory");
+            maybeAddImport("org.springframework.batch.core.repository.JobRepository");
+            maybeRemoveImport("org.springframework.beans.factory.annotation.Autowired");
+            return md;
+        }
+
+        private boolean isJobRepositoryParameter(Statement statement) {
+            return statement instanceof J.VariableDeclarations
+                    && TypeUtils.isOfClassType(((J.VariableDeclarations) statement).getType(),
+                            "org.springframework.batch.core.repository.JobRepository");
+        }
+
+        private boolean isJobBuilderFactoryParameter(Statement statement) {
+            return statement instanceof J.VariableDeclarations
+                    && TypeUtils.isOfClassType(((J.VariableDeclarations) statement).getType(),
+                            "org.springframework.batch.core.configuration.annotation.StepBuilderFactory");
+        }
+    }
+}
---
+++ src/testWithSpringBoot_3_0/java/org/openrewrite/java/spring/batch/MigrateStepBuilderFactoryTest.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.batch;
+
+import static org.openrewrite.java.Assertions.java;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+class MigrateStepBuilderFactoryTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new MigrateStepBuilderFactory())
+                .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(),
+                        "spring-batch-core-4.3.7", "spring-batch-infrastructure-4.3.7", "spring-beans-4.3.30.RELEASE",
+                        "spring-core-4.3.30.RELEASE", "spring-context-4.3.30.RELEASE"));
+    }
+
+    // @Disabled("Disabled until fixed!")
+    @Test
+    void replaceStepBuilderFactoryWithTasket() {
+        // language=java
+        rewriteRun(java("""
+                import org.springframework.batch.core.Step;
+                import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+                import org.springframework.batch.core.step.tasklet.Tasklet;
+                import org.springframework.beans.factory.annotation.Autowired;
+                import org.springframework.context.annotation.Bean;
+
+                public class MyJobConfig {
+
+                    @Autowired
+                    private StepBuilderFactory stepBuilderFactory;
+
+                    @Bean
+                    Step myStep(Tasklet myTasklet) {
+                        return this.stepBuilderFactory.get("myStep")
+                                .tasklet(myTasklet)
+                                .build();
+                    }
+                }
+                """, """
+                import org.springframework.batch.core.Step;
+                import org.springframework.batch.core.repository.JobRepository;
+                import org.springframework.batch.core.step.builder.StepBuilder;
+                import org.springframework.batch.core.step.tasklet.Tasklet;
+                import org.springframework.context.annotation.Bean;
+
+                public class MyJobConfig {
+
+                    @Bean
+                    Step myStep(Tasklet myTasklet, JobRepository jobRepository) {
+                        return new StepBuilder("myStep", jobRepository)
+                                .tasklet(myTasklet)
+                                .build();
+                    }
+                }
+                """));
+    }
+
+    // @Disabled("Disabled until fixed!")
+    @Test
+    void replaceStepBuilderFactoryWithChunk() {
+        // language=java
+        rewriteRun(java("""
+                import org.springframework.batch.core.Step;
+                import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+                import org.springframework.batch.item.ItemReader;
+                import org.springframework.batch.item.ItemWriter;
+                import org.springframework.beans.factory.annotation.Autowired;
+                import org.springframework.context.annotation.Bean;
+
+                public class MyJobConfig {
+
+                    @Autowired
+                    private StepBuilderFactory stepBuilderFactory;
+
+                    @Bean
+                    Step myStep() {
+                        return this.stepBuilderFactory.get("myStep")
+                                .<String, String> chunk(10)
+                                .reader(reader())
+                                .writer(writer())
+                                .build();
+                    }
+
+                    private ItemWriter<String> writer() {
+                        return null;
+                    }
+
+                    private ItemReader<String> reader() {
+                        return null;
+                    }
+                }
+                """, """
+                import org.springframework.batch.core.Step;
+                import org.springframework.batch.core.repository.JobRepository;
+                import org.springframework.batch.core.step.builder.StepBuilder;
+                import org.springframework.batch.item.ItemReader;
+                import org.springframework.batch.item.ItemWriter;
+                import org.springframework.context.annotation.Bean;
+
+                public class MyJobConfig {
+
+                    @Bean
+                    Step myStep(JobRepository jobRepository) {
+                        return new StepBuilder("myStep", jobRepository)
+                                .<String, String> chunk(10)
+                                .reader(reader())
+                                .writer(writer())
+                                .build();
+                    }
+
+                    private ItemWriter<String> writer() {
+                        return null;
+                    }
+
+                    private ItemReader<String> reader() {
+                        return null;
+                    }
+                }
+                """));
+    }
+
+    // @Disabled("Disabled until fixed!")
+    @Test
+    void replaceStepBuilderFactoryWithCompletionPolicyChunk() {
+        // language=java
+        rewriteRun(java("""
+                import org.springframework.batch.core.Step;
+                import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+                import org.springframework.batch.item.ItemReader;
+                import org.springframework.batch.item.ItemWriter;
+                import org.springframework.batch.repeat.CompletionPolicy;
+                import org.springframework.beans.factory.annotation.Autowired;
+                import org.springframework.context.annotation.Bean;
+
+                public class MyJobConfig {
+
+                    @Autowired
+                    private StepBuilderFactory stepBuilderFactory;
+
+                    @Bean
+                    Step myStep() {
+                        return this.stepBuilderFactory.get("myStep")
+                                .<String, String> chunk(completionPolicy())
+                                .reader(reader())
+                                .writer(writer())
+                                .build();
+                    }
+
+                    private CompletionPolicy completionPolicy() {
+                        return null;
+                    }
+
+                    private ItemWriter<String> writer() {
+                        return null;
+                    }
+
+                    private ItemReader<String> reader() {
+                        return null;
+                    }
+                }
+                """, """
+                import org.springframework.batch.core.Step;
+                import org.springframework.batch.core.repository.JobRepository;
+                import org.springframework.batch.core.step.builder.StepBuilder;
+                import org.springframework.batch.item.ItemReader;
+                import org.springframework.batch.item.ItemWriter;
+                import org.springframework.batch.repeat.CompletionPolicy;
+                import org.springframework.context.annotation.Bean;
+
+                public class MyJobConfig {
+
+                    @Bean
+                    Step myStep(JobRepository jobRepository) {
+                        return new StepBuilder("myStep", jobRepository)
+                                .<String, String> chunk(completionPolicy())
+                                .reader(reader())
+                                .writer(writer())
+                                .build();
+                    }
+
+                    private CompletionPolicy completionPolicy() {
+                        return null;
+                    }
+
+                    private ItemWriter<String> writer() {
+                        return null;
+                    }
+
+                    private ItemReader<String> reader() {
+                        return null;
+                    }
+                }
+                """));
+    }
+
+}
