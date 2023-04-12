87a2ca50f357ae7e402d5ab656309f72c23ee85d
Knut Wannheden <knut@moderne.io>
2023-04-12 17:24:31 +0200

    Add missing copyright header
    Issue: #299

--- src/test/java/org/openrewrite/java/spring/cloud2022/MigrateProjectTest.java
+++ src/test/java/org/openrewrite/java/spring/cloud2022/MigrateProjectTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.spring.cloud2022;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +24,8 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.*;
 import static org.openrewrite.maven.Assertions.pomXml;
+import static org.openrewrite.properties.Assertions.properties;
+import static org.openrewrite.yaml.Assertions.yaml;
 
 public class MigrateProjectTest implements RewriteTest {
 
@@ -76,6 +93,35 @@ public class MigrateProjectTest implements RewriteTest {
                         </dependency>
                     </dependencies>
                 </project>
+                """
+            )
+          )
+        );
+    }
+
+    @Test
+    void migrateProperties() {
+        rewriteRun(
+          mavenProject("project",
+            //language=properties
+            properties(
+              """
+                spring.sleuth.baggage.correlation-enabled=true
+                """,
+              """
+                management.tracing.baggage.correlation.enabled=true
+                """
+            ),
+            //language=yaml
+            yaml(
+              """
+                spring:
+                    sleuth:
+                        baggage:
+                            correlation-enabled: true
+                """,
+              """
+                management.tracing.baggage.correlation.enabled: true
                 """
             )
           )
