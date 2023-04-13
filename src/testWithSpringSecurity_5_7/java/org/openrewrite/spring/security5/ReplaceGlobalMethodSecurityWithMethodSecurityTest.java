eb2f0588e41086f0693d77cbdf219489b5b387b5
Kun Li <kun@moderne.io>
2023-04-12 23:28:30 -0700

    Add recipe ReplaceGlobalMethodSecurityWithMethodSecurityXml

--- src/testWithSpringSecurity_5_7/java/org/openrewrite/spring/security5/ReplaceGlobalMethodSecurityWithMethodSecurityTest.java
+++ src/testWithSpringSecurity_5_7/java/org/openrewrite/spring/security5/ReplaceGlobalMethodSecurityWithMethodSecurityTest.java
@@ -2,12 +2,14 @@ package org.openrewrite.spring.security5;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.config.Environment;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.spring.security5.ReplaceGlobalMethodSecurityWithMethodSecurity;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.xml.Assertions.xml;
 
 public class ReplaceGlobalMethodSecurityWithMethodSecurityTest implements RewriteTest {
     @Override
@@ -57,6 +59,39 @@ public class ReplaceGlobalMethodSecurityWithMethodSecurityTest implements Rewrit
               @EnableMethodSecurity(securedEnabled = true, prePostEnabled = false)
               public class config {
               }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeUseAuthorizationManagerAttribute() {
+        rewriteRun(
+          spec -> spec.recipe(
+            Environment.builder()
+              .scanRuntimeClasspath()
+              .build()
+              .activateRecipes("org.openrewrite.java.spring.security5.ReplaceGlobalMethodSecurityWithMethodSecurityXml")
+          ),
+          //language=xml
+          xml(
+            """
+              <b:beans xmlns:b="http://www.springframework.org/schema/beans"
+              		 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              		 xmlns="http://www.springframework.org/schema/security"
+              		 xsi:schemaLocation="http://www.springframework.org/schema/security https://www.springframework.org/schema/security/spring-security.xsd
+              		http://www.springframework.org/schema/beans https://www.springframework.org/schema/beans/spring-beans.xsd">
+                <global-method-security pre-post-enabled="true"/>
+              </b:beans>
+              """,
+            """
+              <b:beans xmlns:b="http://www.springframework.org/schema/beans"
+              		 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              		 xmlns="http://www.springframework.org/schema/security"
+              		 xsi:schemaLocation="http://www.springframework.org/schema/security https://www.springframework.org/schema/security/spring-security.xsd
+              		http://www.springframework.org/schema/beans https://www.springframework.org/schema/beans/spring-beans.xsd">
+                <method-security/>
+              </b:beans>
               """
           )
         );
