38f047ec8482d11e45df66689c133f41f2d920ac
Nick McKinney <mckinneynichoals@gmail.com>
2023-05-23 14:14:36 -0400

    fixing test case in response to rewrite#3268

--- src/test/java/org/openrewrite/java/spring/cloud2022/MigrateProjectTest.java
+++ src/test/java/org/openrewrite/java/spring/cloud2022/MigrateProjectTest.java
@@ -187,7 +187,8 @@ class MigrateProjectTest implements RewriteTest {
               spec -> spec.after(after -> {
                   Matcher matcher = Pattern.compile("            <version>(.*)</version>").matcher(after);
                   assertThat(matcher.find()).describedAs(after).isTrue();
-                  String version = matcher.group(1);
+                  String micrometerVersion = matcher.group(1);
+                  String springBootVersion = matcher.group(2);
                   return """
                         <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
                              xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
@@ -207,20 +208,21 @@ class MigrateProjectTest implements RewriteTest {
                             <dependency>
                                 <groupId>io.micrometer</groupId>
                                 <artifactId>micrometer-tracing</artifactId>
+                                <version>%1$s</version>
                             </dependency>
                             <dependency>
                                 <groupId>org.springframework.boot</groupId>
                                 <artifactId>spring-boot-starter-actuator</artifactId>
-                                <version>%1$s</version>
+                                <version>%2$s</version>
                             </dependency>
                             <dependency>
                                 <groupId>org.springframework.boot</groupId>
                                 <artifactId>spring-boot-starter-aop</artifactId>
-                                <version>%1$s</version>
+                                <version>%2$s</version>
                             </dependency>
                         </dependencies>
                     </project>
-                    """.formatted(version);
+                    """.formatted(micrometerVersion, springBootVersion);
               })
             )
           )
