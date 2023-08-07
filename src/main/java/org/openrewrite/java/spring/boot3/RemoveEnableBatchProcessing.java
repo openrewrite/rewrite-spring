5514dc5be82ecba1137ee1d4cca58ffe9b7999fc
Tim te Beek <tim@moderne.io>
2023-08-07 10:33:38 +0200

    Also remove EnableBatchProcessing import
    Fixes #406

--- src/main/java/org/openrewrite/java/spring/boot3/RemoveEnableBatchProcessing.java
+++ src/main/java/org/openrewrite/java/spring/boot3/RemoveEnableBatchProcessing.java
@@ -16,11 +16,13 @@
 package org.openrewrite.java.spring.boot3;
 
 import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.search.FindAnnotations;
+import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.TypeUtils;
 
@@ -36,16 +38,18 @@ public class RemoveEnableBatchProcessing extends Recipe {
         return "Add or remove the `@EnableBatchProcessing` annotation from a Spring Boot application.";
     }
 
+    private static final String ENABLE_BATCH_PROCESSING = "org.springframework.batch.core.configuration.annotation.EnableBatchProcessing";
+
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return new JavaIsoVisitor<ExecutionContext>() {
-
+        return Preconditions.check(new UsesType<>(ENABLE_BATCH_PROCESSING, true), new JavaIsoVisitor<ExecutionContext>() {
             @Override
             public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
                 if (!FindAnnotations.find(classDecl, "@org.springframework.boot.autoconfigure.SpringBootApplication").isEmpty() &&
-                    !FindAnnotations.find(classDecl, "@org.springframework.batch.core.configuration.annotation.EnableBatchProcessing").isEmpty()) {
+                    !FindAnnotations.find(classDecl, "@" + ENABLE_BATCH_PROCESSING).isEmpty()) {
                     return classDecl.withLeadingAnnotations(ListUtils.map(classDecl.getLeadingAnnotations(), a -> {
-                        if (TypeUtils.isOfClassType(a.getType(), "org.springframework.batch.core.configuration.annotation.EnableBatchProcessing")) {
+                        if (TypeUtils.isOfClassType(a.getType(), ENABLE_BATCH_PROCESSING)) {
+                            maybeRemoveImport(ENABLE_BATCH_PROCESSING);
                             return null;
                         }
                         return a;
@@ -53,6 +57,6 @@ public class RemoveEnableBatchProcessing extends Recipe {
                 }
                 return super.visitClassDeclaration(classDecl, ctx);
             }
-        };
+        });
     }
 }
