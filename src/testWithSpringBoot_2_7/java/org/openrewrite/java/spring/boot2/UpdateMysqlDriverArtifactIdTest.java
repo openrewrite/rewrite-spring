a1a64e1e9d40c526ab7c48c84f2b94eb22ba9bf8
Kun Li <kun@moderne.io>
2023-09-07 12:49:44 -0700

    Revert "Limit scanned classpath for UpdateMysqlDriverArtifactIdTest"
    This reverts commit 63274633b80ac9bd0bf152b4b39051dba551d943.

--- src/testWithSpringBoot_2_7/java/org/openrewrite/java/spring/boot2/UpdateMysqlDriverArtifactIdTest.java
+++ src/testWithSpringBoot_2_7/java/org/openrewrite/java/spring/boot2/UpdateMysqlDriverArtifactIdTest.java
@@ -32,12 +32,12 @@ import static org.openrewrite.gradle.Assertions.buildGradle;
 import static org.openrewrite.maven.Assertions.pomXml;
 
 @Issue("https://github.com/openrewrite/rewrite-spring/issues/274")
-class UpdateMysqlDriverArtifactIdTest implements RewriteTest {
+public class UpdateMysqlDriverArtifactIdTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
         spec.recipe(Environment.builder()
-          .scanRuntimeClasspath("org.openrewrite.java.spring.boot2")
+          .scanRuntimeClasspath()
           .build()
           .activateRecipes("org.openrewrite.java.spring.boot2.UpgradeSpringBoot_2_7"));
     }
