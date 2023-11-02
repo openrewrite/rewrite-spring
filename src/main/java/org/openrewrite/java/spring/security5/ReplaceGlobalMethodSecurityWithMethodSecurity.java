openrewrite/rewrite-spring

commit ba677a94a8564aac11da072a56f69742b65122de

Fix NPE in `ReplaceGlobalMethodSecurityWithMethodSecurity`

src/main/java/org/openrewrite/java/spring/security5/ReplaceGlobalMethodSecurityWithMethodSecurity.java
@@ -26,6 +26,7 @@ import org.openrewrite.java.spring.RemoveMethodInvocationsVisitor;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -59,13 +60,12 @@ public class ReplaceGlobalMethodSecurityWithMethodSecurity extends Recipe {
                 annotation = super.visitAnnotation(annotation, ctx);
                 if (ENABLE_GLOBAL_METHOD_SECURITY_MATCHER.matches(annotation)) {
                     List<Expression> args = annotation.getArguments();
-                    boolean hasPrePostEnabled = args.stream().anyMatch(this::hasPrePostEnabled);
+                    boolean hasPrePostEnabled = args != null && args.stream().anyMatch(this::hasPrePostEnabled);
                     List<Expression> newArgs;
                     if (hasPrePostEnabled) {
                         newArgs = args.stream().filter(arg -> !hasPrePostEnabled(arg)).collect(Collectors.toList());
                     } else {
-                        newArgs = args;
-                        newArgs.add(buildPrePostEnabledAssignedToFalse());
+                        newArgs = Collections.singletonList(buildPrePostEnabledAssignedToFalse());
                     }
 
                     maybeAddImport(EnableMethodSecurityFqn);

src/testWithSpringSecurity_5_7/java/org/openrewrite/spring/security5/ReplaceGlobalMethodSecurityWithMethodSecurityTest.java
@@ -60,6 +60,28 @@ public class ReplaceGlobalMethodSecurityWithMethodSecurityTest implements Rewrit
     }
 
     @Test
+    void emptyAnnotation() {
+        rewriteRun(
+          java(
+            """
+              import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+              @EnableGlobalMethodSecurity
+              public class config {
+              }
+              """,
+            """
+              import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+              @EnableMethodSecurity(prePostEnabled = false)
+              public class config {
+              }
+              """
+          )
+        );
+    }
+
+    @Test
     void replaceWithNotPrePostEnabled() {
         rewriteRun(
           java(

commit 0f684b8f3a6d940e05bd066d10ec7c36c6822fcb

Fix bug in `ReplaceGlobalMethodSecurityWithMethodSecurity`
Introduced in ba677a94a8564aac11da072a56f69742b65122de.

src/main/java/org/openrewrite/java/spring/security5/ReplaceGlobalMethodSecurityWithMethodSecurity.java
@@ -54,16 +54,21 @@ public class ReplaceGlobalMethodSecurityWithMethodSecurity extends Recipe {
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
         return Preconditions.check(new UsesType<>(
-                "org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity", false), new JavaIsoVisitor<ExecutionContext>() {
+                "org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity", false
+        ), new JavaIsoVisitor<ExecutionContext>() {
             @Override
             public J.Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
                 annotation = super.visitAnnotation(annotation, ctx);
                 if (ENABLE_GLOBAL_METHOD_SECURITY_MATCHER.matches(annotation)) {
                     List<Expression> args = annotation.getArguments();
-                    boolean hasPrePostEnabled = args != null && args.stream().anyMatch(this::hasPrePostEnabled);
                     List<Expression> newArgs;
-                    if (hasPrePostEnabled) {
-                        newArgs = args.stream().filter(arg -> !hasPrePostEnabled(arg)).collect(Collectors.toList());
+                    if (args != null && !args.isEmpty()) {
+                        newArgs = args;
+                        if (args.stream().noneMatch(this::hasPrePostEnabled)) {
+                            newArgs.add(buildPrePostEnabledAssignedToFalse());
+                        } else {
+                            newArgs = args.stream().filter(arg -> !hasPrePostEnabled(arg)).collect(Collectors.toList());
+                        }
                     } else {
                         newArgs = Collections.singletonList(buildPrePostEnabledAssignedToFalse());
                     }
@@ -87,7 +92,7 @@ public class ReplaceGlobalMethodSecurityWithMethodSecurity extends Recipe {
             private boolean hasPrePostEnabled(Expression arg) {
                 if (arg instanceof J.Assignment) {
                     J.Assignment assignment = (J.Assignment) arg;
-                    return assignment.getVariable().toString().equals("prePostEnabled") &&
+                    return ((J.Identifier) assignment.getVariable()).getSimpleName().equals("prePostEnabled") &&
                             RemoveMethodInvocationsVisitor.isTrue(assignment.getAssignment());
                 }
                 return false;