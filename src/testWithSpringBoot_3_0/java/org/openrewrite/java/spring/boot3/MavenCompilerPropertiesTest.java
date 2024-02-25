7e81d73bea7df546790f6d441dbeb735500d7dce
Tim te Beek <timtebeek@gmail.com>
2024-02-25 21:15:13 +0100

    Update src/testWithSpringBoot_3_0/java/org/openrewrite/java/spring/boot3/MavenCompilerPropertiesTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/testWithSpringBoot_3_0/java/org/openrewrite/java/spring/boot3/MavenCompilerPropertiesTest.java
+++ src/testWithSpringBoot_3_0/java/org/openrewrite/java/spring/boot3/MavenCompilerPropertiesTest.java
@@ -1,10 +1,28 @@
-package org.openrewrite.java.spring.boot3;
-
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.openrewrite.DocumentExample;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.junit.jupiter.api.Test;
-import org.openrewrite.config.Environment;
+class MavenCompilerPropertiesTest implements RewriteTest {
+    @DocumentExample
+    @Test
+    void upgradeGradleWrapperAndPlugins() {
+		  pomXml(
+                """
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.maven.MavenParser;
 import org.openrewrite.test.RecipeSpec;
