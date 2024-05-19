6b481aa8eb78e9ed7c0f7cf7c63797270e965d57
Tim te Beek <tim@moderne.io>
2024-05-19 12:50:47 +0200

    Expand use of .classpathWithResources

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,6 +77,7 @@ recipeDependencies {
     parserClasspath("org.springframework:spring-web:5.+")
     parserClasspath("org.springframework:spring-web:6.+")
 
+    parserClasspath("org.springframework:spring-webflux:5.+")
     parserClasspath("org.springframework:spring-webmvc:5.+")
 
     parserClasspath("org.springframework.data:spring-data-commons:2.+")
