be3c18edb9e6c0339e8ce74f999721384e53df40
Tim Büthe <t.buethe@mytaxi.com>
2024-06-21 10:15:53 +0200

    #537 apply patch from PR to fix formatting and set expected cycles: 2
    https://github.com/openrewrite/rewrite-spring/pull/539#issuecomment-2180541805

--- src/testWithSpringBoot_3_3/java/org/openrewrite/java/spring/boot3/SpringCloudVersionUpgradeTest.java
+++ src/testWithSpringBoot_3_3/java/org/openrewrite/java/spring/boot3/SpringCloudVersionUpgradeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2024 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,16 +26,16 @@ import static org.openrewrite.maven.Assertions.pomXml;
 
 class SpringCloudVersionUpgradeTest implements RewriteTest {
 
-
     @Override
     public void defaults(RecipeSpec spec) {
         spec
+          .expectedCyclesThatMakeChanges(2)
           .recipe(Environment.builder()
             .scanRuntimeClasspath("org.openrewrite.java.spring")
             .build()
             .activateRecipes("org.openrewrite.java.spring.boot3.UpgradeSpringBoot_3_3")
           ).parser(JavaParser.fromJavaVersion()
-            .classpath( "jaxb-api"));
+            .classpath("jaxb-api"));
     }
 
     @Test
@@ -45,82 +45,81 @@ class SpringCloudVersionUpgradeTest implements RewriteTest {
             //language=xml
             pomXml(
               """
-                 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-                     <modelVersion>4.0.0</modelVersion>
-                
-                     <groupId>com.example</groupId>
-                     <artifactId>fooservice</artifactId>
-                     <version>1.0-SNAPSHOT</version>
-                
-                     <name>FooService</name>
-                
-                     <parent>
-                         <groupId>org.springframework.boot</groupId>
-                         <artifactId>spring-boot-starter-parent</artifactId>
-                         <version>2.2.2.RELEASE</version>
-                         <relativePath/>
-                     </parent>
-                
-                     <properties>
-                         <java.version>11</java.version>
-                         <spring-cloud.version>Hoxton.SR9</spring-cloud.version>
-                         <mockito.version>2.18.3</mockito.version>
-                     </properties>
-                
-                     <dependencyManagement>
-                         <dependencies>
-                             <dependency>
-                                 <groupId>org.springframework.cloud</groupId>
-                                 <artifactId>spring-cloud-dependencies</artifactId>
-                                 <version>${spring-cloud.version}</version>
-                                 <type>pom</type>
-                                 <scope>import</scope>
-                             </dependency>
-                         </dependencies>
-                     </dependencyManagement>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+
+                    <groupId>com.example</groupId>
+                    <artifactId>fooservice</artifactId>
+                    <version>1.0-SNAPSHOT</version>
+
+                    <name>FooService</name>
+
+                    <parent>
+                        <groupId>org.springframework.boot</groupId>
+                        <artifactId>spring-boot-starter-parent</artifactId>
+                        <version>2.2.2.RELEASE</version>
+                        <relativePath/>
+                    </parent>
+
+                    <properties>
+                        <java.version>11</java.version>
+                        <spring-cloud.version>Hoxton.SR9</spring-cloud.version>
+                        <mockito.version>2.18.3</mockito.version>
+                    </properties>
+
+                    <dependencyManagement>
+                        <dependencies>
+                            <dependency>
+                                <groupId>org.springframework.cloud</groupId>
+                                <artifactId>spring-cloud-dependencies</artifactId>
+                                <version>${spring-cloud.version}</version>
+                                <type>pom</type>
+                                <scope>import</scope>
+                            </dependency>
+                        </dependencies>
+                    </dependencyManagement>
                 </project>
                 """,
               """
                 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-                     <modelVersion>4.0.0</modelVersion>
-                
-                     <groupId>com.example</groupId>
-                     <artifactId>fooservice</artifactId>
-                     <version>1.0-SNAPSHOT</version>
-                
-                     <name>FooService</name>
-                
-                     <parent>
-                         <groupId>org.springframework.boot</groupId>
-                         <artifactId>spring-boot-starter-parent</artifactId>
-                         <version>3.3.0</version>
-                         <relativePath/>
-                     </parent>
-                
-                     <properties>
+                         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+
+                    <groupId>com.example</groupId>
+                    <artifactId>fooservice</artifactId>
+                    <version>1.0-SNAPSHOT</version>
+
+                    <name>FooService</name>
+
+                    <parent>
+                        <groupId>org.springframework.boot</groupId>
+                        <artifactId>spring-boot-starter-parent</artifactId>
+                        <version>3.3.1</version>
+                        <relativePath/>
+                    </parent>
+
+                    <properties>
                         <java.version>17</java.version>
                         <spring-cloud.version>2023.0.2</spring-cloud.version>
                         <mockito.version>2.18.3</mockito.version>
-                     </properties>
-                
-                     <dependencyManagement>
-                         <dependencies>
-                             <dependency>
-                                 <groupId>org.springframework.cloud</groupId>
-                                 <artifactId>spring-cloud-dependencies</artifactId>
-                                 <version>${spring-cloud.version}</version>
-                                 <type>pom</type>
-                                 <scope>import</scope>
-                             </dependency>
-                         </dependencies>
-                     </dependencyManagement>
+                    </properties>
+
+                    <dependencyManagement>
+                        <dependencies>
+                            <dependency>
+                                <groupId>org.springframework.cloud</groupId>
+                                <artifactId>spring-cloud-dependencies</artifactId>
+                                <version>${spring-cloud.version}</version>
+                                <type>pom</type>
+                                <scope>import</scope>
+                            </dependency>
+                        </dependencies>
+                    </dependencyManagement>
                 </project>
                 """
             )
           )
         );
     }
-
 }
