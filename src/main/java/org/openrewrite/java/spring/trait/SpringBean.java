16f82edfb6d8effcefc1c285ab0ca5bc16b1c905
Jonathan Schneider <jkschneider@gmail.com>
2024-07-10 13:44:00 -0400

    SpringBean trait

---
+++ src/main/java/org/openrewrite/java/spring/trait/SpringBean.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.trait;
+
+import lombok.Value;
+import org.openrewrite.Cursor;
+import org.openrewrite.Tree;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.trait.SimpleTraitMatcher;
+import org.openrewrite.trait.Trait;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.tree.Xml;
+
+import java.util.Optional;
+
+import static org.openrewrite.java.trait.Traits.annotated;
+
+@Value
+public class SpringBean implements Trait<Tree> {
+    Cursor cursor;
+
+    @Nullable
+    public String getName() {
+        if (getTree() instanceof Xml.Tag) {
+            Xml.Tag tag = (Xml.Tag) getTree();
+            return tag.getAttributes().stream()
+                    .filter(a -> "id".equals(a.getName()))
+                    .findFirst()
+                    .map(Xml.Attribute::getValueAsString)
+                    .orElse(null);
+        } else if (getTree() instanceof J.Annotation) {
+            J.Annotation annotation = (J.Annotation) getTree();
+            return annotated("org.springframework.context.annotation.Bean")
+                    .get(cursor)
+                    .flatMap(a -> a.getDefaultAttribute("name"))
+                    .map(name -> name.getValue(String.class))
+                    .orElse(null);
+        }
+        return null;
+    }
+
+    public Optional<J.MethodDeclaration> getBeanMethod() {
+        if (getTree() instanceof J.MethodDeclaration) {
+            return Optional.of((J.MethodDeclaration) getTree());
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Xml.Tag> getXmlBeanConfiguration() {
+        if (getTree() instanceof Xml.Tag) {
+            return Optional.of((Xml.Tag) getTree());
+        }
+        return Optional.empty();
+    }
+
+    public static class Matcher extends SimpleTraitMatcher<SpringBean> {
+        @Override
+        protected @Nullable SpringBean test(Cursor cursor) {
+            Object value = cursor.getValue();
+            if (value instanceof Xml.Tag) {
+                XPathMatcher springBean = new XPathMatcher("/beans/bean");
+                if (springBean.matches(cursor)) {
+                    return new SpringBean(cursor);
+                }
+            } else if (value instanceof J.Annotation) {
+                AnnotationMatcher beanAnnotation = new AnnotationMatcher("@org.springframework.context.annotation.Bean");
+                if (beanAnnotation.matches((J.Annotation) value)) {
+                    return new SpringBean(cursor.getParentTreeCursor());
+                }
+            }
+            return null;
+        }
+    }
+}
