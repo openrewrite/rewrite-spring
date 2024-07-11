be680e72de3d6dbb92d1d1816aa33b90f18848f8
Dariy Pozinenko <33901469+dpozinen@users.noreply.github.com>
2024-07-11 16:35:35 +0300

    refactor
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/java/spring/boot2/MoveAutoConfigurationToImportsFile.java
+++ src/main/java/org/openrewrite/java/spring/boot2/MoveAutoConfigurationToImportsFile.java
@@ -29,16 +29,7 @@ import org.openrewrite.text.PlainText;
 import org.openrewrite.text.PlainTextParser;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
