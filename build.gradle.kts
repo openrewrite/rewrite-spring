7ade8a85a135d3bcff8dff12bdae69ce4f1f538e
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-08-12 17:51:24 +0200

    Include new reactor migration as of springboot 3.2 Spring boot upgrade to 3.2.4 also upgrades io.projectreactor/reactor-bom from 2020.0.27 to 2023.0.4. This in turn upgrades reactor-core module from 3.4.26 to 3.6.5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,6 +123,7 @@ dependencies {
 
     runtimeOnly("org.openrewrite:rewrite-java-17")
     runtimeOnly("org.openrewrite.recipe:rewrite-hibernate:$rewriteVersion")
+    runtimeOnly("org.openrewrite.recipe:rewrite-reactive-streams:$rewriteVersion")
     runtimeOnly("org.openrewrite.recipe:rewrite-migrate-java:$rewriteVersion")
     runtimeOnly("org.openrewrite.recipe:rewrite-openapi:${rewriteVersion}")
     runtimeOnly("org.openrewrite.recipe:rewrite-testing-frameworks:$rewriteVersion")
