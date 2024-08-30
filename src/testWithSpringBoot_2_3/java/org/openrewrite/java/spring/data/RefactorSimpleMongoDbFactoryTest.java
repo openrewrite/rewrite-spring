6dd5d31d0f0c30a52db388580123138498d075d0
Tim te Beek <tim@moderne.io>
2024-08-30 16:46:53 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/testWithSpringBoot_2_3/java/org/openrewrite/java/spring/data/RefactorSimpleMongoDbFactoryTest.java
+++ src/testWithSpringBoot_2_3/java/org/openrewrite/java/spring/data/RefactorSimpleMongoDbFactoryTest.java
@@ -23,7 +23,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
-public class RefactorSimpleMongoDbFactoryTest implements RewriteTest {
+class RefactorSimpleMongoDbFactoryTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
