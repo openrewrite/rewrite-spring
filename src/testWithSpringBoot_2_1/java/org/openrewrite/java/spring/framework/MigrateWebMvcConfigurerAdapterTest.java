9e96f88a998dcf8c427896d217d7e503c45585e3
Sam Snyder <sam@moderne.io>
2024-09-05 13:23:59 -0700

    Polish

--- src/testWithSpringBoot_2_1/java/org/openrewrite/java/spring/framework/MigrateWebMvcConfigurerAdapterTest.java
+++ src/testWithSpringBoot_2_1/java/org/openrewrite/java/spring/framework/MigrateWebMvcConfigurerAdapterTest.java
@@ -25,6 +25,7 @@ import org.openrewrite.test.RewriteTest;
 import static org.openrewrite.java.Assertions.java;
 
 class MigrateWebMvcConfigurerAdapterTest implements RewriteTest {
+
     @Override
     public void defaults(RecipeSpec spec) {
         spec.parser(JavaParser.fromJavaVersion()
