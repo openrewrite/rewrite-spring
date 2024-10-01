14a48ce9ea3fe0acb15d22275a90973e5a269bd6
Nate Danner <nate@moderne.io>
2024-10-01 16:33:59 -0700

    update to latest error prone to remove protobuf-java@3.19.2 vulnerability (#597)

--- build.gradle.kts
+++ build.gradle.kts
@@ -141,7 +141,7 @@ dependencies {
     runtimeOnly("org.openrewrite.recipe:rewrite-testing-frameworks:$rewriteVersion")
 
     annotationProcessor("org.openrewrite:rewrite-templating:$rewriteVersion")
-    compileOnly("com.google.errorprone:error_prone_core:2.19.1") {
+    compileOnly("com.google.errorprone:error_prone_core:2.+") {
         exclude("com.google.auto.service", "auto-service-annotations")
     }
     implementation("org.mongodb:mongo-java-driver:3.12.+")
