28b79a8d30823096d90f4bf03dcbedde8b7838bd
SiBorea <siborea.fl@gmail.com>
2024-11-06 17:59:38 +0800

    Add BeanMethodReturnNull recipe

---
+++ src/main/java/org/openrewrite/java/spring/boot3/BeanMethodReturnNull.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.boot3;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.NlsRewrite;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.Statement;
+import org.openrewrite.java.tree.TypeTree;
+import org.openrewrite.java.tree.TypeUtils;
+
+import java.util.List;
+
+public class BeanMethodReturnNull extends Recipe {
+    protected static final String BEAN_ANNOTATION_FQN = "org.springframework.context.annotation.Bean";
+    protected static final AnnotationMatcher BEAN_ANNOTATION_MATCHER = new AnnotationMatcher("@org.springframework.context.annotation.Bean");
+    private static final String MSG_RETURN_VOID = "RETURN_VOID";
+
+    @Override
+    public String getDisplayName() {
+        return "Make all @Bean method to return non-void (null)";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Make all @Bean method to return non-void (null).";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(new UsesType<>(BEAN_ANNOTATION_FQN, false),
+          new BeanMethodReturnVoidVisitor());
+    }
+
+    private static class BeanMethodReturnVoidVisitor extends JavaIsoVisitor<ExecutionContext> {
+        @Override
+        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration md, ExecutionContext ctx) {
+            // When the method is annotated with @Bean and it is not an override method
+            if (md.getAllAnnotations().stream().anyMatch(BEAN_ANNOTATION_MATCHER::matches) &&
+              !TypeUtils.isOverride(md.getMethodType()) &&
+              md.getReturnTypeExpression() != null && md.getReturnTypeExpression().getType() == JavaType.Primitive.Void
+            ) {
+                md = md.withReturnTypeExpression(TypeTree.build(" Object"));
+
+                getCursor().putMessage(MSG_RETURN_VOID, true);
+
+                // Add `return null;` if the method does not have a return statement
+                List<Statement> statements = md.getBody().getStatements();
+                if (statements.isEmpty() || !(statements.get(statements.size() - 1) instanceof J.Return)) {
+                    md = JavaTemplate.builder("return null;")
+                      .contextSensitive()
+                      .build()
+                      .apply(updateCursor(md), md.getBody().getCoordinates().lastStatement());
+                }
+            }
+
+            return super.visitMethodDeclaration(md, ctx);
+        }
+
+        // Change `return;` to `return null;`
+        @Override
+        public J.Return visitReturn(J.Return _return, ExecutionContext ctx) {
+            if (_return.getExpression() == null) {
+                boolean isReturnVoid = Boolean.TRUE.equals(getCursor().getNearestMessage(MSG_RETURN_VOID));
+                if (isReturnVoid) {
+                    _return = JavaTemplate.builder("return null;")
+                      .contextSensitive()
+                      .build()
+                      .apply(updateCursor(_return), _return.getCoordinates().replace());
+                }
+            }
+            return super.visitReturn(_return, ctx);
+        }
+    }
+}
+
---
+++ src/testWithSpringBoot_3_0/java/org/openrewrite/java/spring/boot3/BeanMethodReturnVoidTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.boot3;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import static org.openrewrite.java.Assertions.java;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+public class BeanMethodReturnVoidTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new BeanMethodReturnNull())
+          .parser(JavaParser.fromJavaVersion().classpath("spring-context"));
+    }
+
+    @DocumentExample
+    @Test
+    void transformReturnType() {
+        // language=java
+        rewriteRun(
+          java(
+            """
+            import org.springframework.context.annotation.Bean;
+
+            public class Test {
+
+                @Bean
+                public void myBean() {
+                }
+            }
+        """, """
+            import org.springframework.context.annotation.Bean;
+
+            public class Test {
+
+                @Bean
+                public Object myBean() {
+                    return null;
+                }
+            }
+            """)
+        );
+    }
+
+    @Test
+    void transformAllReturn() {
+        // language=java
+        rewriteRun(
+          java(
+            """
+            import org.springframework.context.annotation.Bean;
+
+            public class Test {
+
+                @Bean
+                public void myBean() {
+                    if (true) {
+                        return;
+                    }
+                    int i = 1;
+                    return;
+                }
+
+                @Bean
+                public Object foo() {
+                    return "foo";
+                }
+            }
+        """, """
+            import org.springframework.context.annotation.Bean;
+
+            public class Test {
+
+                @Bean
+                public Object myBean() {
+                    if (true) {
+                        return null;
+                    }
+                    int i = 1;
+                    return null;
+                }
+
+                @Bean
+                public Object foo() {
+                    return "foo";
+                }
+            }
+            """)
+        );
+    }
+}
