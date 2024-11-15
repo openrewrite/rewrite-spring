9511bed3b3ebd81acb457c789e7ca15f9af58a52
Tim te Beek <tim@moderne.io>
2024-11-15 21:09:01 +0100

    Reorder import statements in test file

--- src/testWithSpringBoot_2_4/java/org/openrewrite/java/spring/boot2/HeadersConfigurerLambdaDslTest.java
+++ src/testWithSpringBoot_2_4/java/org/openrewrite/java/spring/boot2/HeadersConfigurerLambdaDslTest.java
@@ -18,10 +18,11 @@ package org.openrewrite.java.spring.boot2;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 
-import static org.openrewrite.java.Assertions.java;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
 
 class HeadersConfigurerLambdaDslTest implements RewriteTest {
     @Override
