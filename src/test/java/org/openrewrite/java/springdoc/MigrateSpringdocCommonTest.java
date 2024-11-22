506062b87c8cfd34c16ff811f251da5d20f5d751
SiBorea <siborea.fl@gmail.com>
2024-11-22 10:24:18 +0800

    Fix syntax messed up by bot

--- src/test/java/org/openrewrite/java/springdoc/MigrateSpringdocCommonTest.java
+++ src/test/java/org/openrewrite/java/springdoc/MigrateSpringdocCommonTest.java
@@ -18,8 +18,7 @@ package org.openrewrite.java.springdoc;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 import static org.openrewrite.java.Assertions.java;
-
-class MigrateSpringdocCommonTest implements RewriteTest {
+import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 import org.openrewrite.test.TypeValidation;
@@ -34,8 +33,8 @@ public class MigrateSpringdocCommonTest implements RewriteTest {
           ));
     }
 
+    @Test
     @DocumentExample
-    void fixCustomiserAndGroupedOpenApi() {
     public void fixCustomiserAndGroupedOpenApi() {
         // language=java
         rewriteRun(
