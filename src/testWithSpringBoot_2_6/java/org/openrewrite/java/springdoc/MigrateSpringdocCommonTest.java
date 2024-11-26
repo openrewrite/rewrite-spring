4d67107e8ef650326881f959a2dcb2b0d8bcf143
SiBorea <108953913+SiBorea@users.noreply.github.com>
2024-11-27 01:47:12 +0800

    Add MigrateSpringdocCommon recipe (#633)
    * Add MigrateSpringdocCommon recipe
    
    * Apply suggestions from bot
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Fix syntax messed up by bot
    
    * Add MigrateSpringdocCommon recipe
    
    * Apply suggestions from bot
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Fix syntax messed up by bot
    
    * Fix build fail on SpringBoot_1_5
    
    * Fix rebase error
    
    * Move to Spring Boot 2.6 tests, to match inclusion in `spring-boot-26.yml`
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/testWithSpringBoot_2_6/java/org/openrewrite/java/springdoc/MigrateSpringdocCommonTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.springdoc;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
+import static org.openrewrite.java.Assertions.java;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+import org.openrewrite.test.TypeValidation;
+
+class MigrateSpringdocCommonTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("org.openrewrite.java.springdoc.MigrateSpringdocCommon")
+          .parser(JavaParser.fromJavaVersion().classpath(
+            "springdoc-openapi-common-1.+",
+            "swagger-models-2.+"
+          ));
+    }
+
+    @Test
+    @DocumentExample
+    void fixCustomiserAndGroupedOpenApi() {
+        // language=java
+        rewriteRun(
+          java(
+            """
+              import io.swagger.v3.oas.models.OpenAPI;
+              import org.springdoc.core.GroupedOpenApi;
+              import org.springdoc.core.customizers.OpenApiCustomiser;
+
+              public class OpenApiConfiguration {
+
+                  public static void groupedOpenApi() {
+                      GroupedOpenApi.builder()
+                        .group("group")
+                        .pathsToMatch("/api/**")
+                        .addOpenApiCustomiser(new FoobarOpenApiCustomiser())
+                        .build();
+                  }
+
+                  public static class FoobarOpenApiCustomiser implements OpenApiCustomiser {
+                      @Override
+                      public void customise(OpenAPI openApi) {
+                      }
+                  }
+              }
+              """, """
+              import io.swagger.v3.oas.models.OpenAPI;
+              import org.springdoc.core.customizers.OpenApiCustomizer;
+              import org.springdoc.core.models.GroupedOpenApi;
+
+              public class OpenApiConfiguration {
+
+                  public static void groupedOpenApi() {
+                      GroupedOpenApi.builder()
+                        .group("group")
+                        .pathsToMatch("/api/**")
+                        .addOpenApiCustomizer(new FoobarOpenApiCustomiser())
+                        .build();
+                  }
+
+                  public static class FoobarOpenApiCustomiser implements OpenApiCustomizer {
+                      @Override
+                      public void customise(OpenAPI openApi) {
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+}
