1f8866c93ad0be17e1acdf0494142ea8af96ba39
chao.wang <chao.wang@zatech.com>
2025-03-27 17:55:38 +0800

    Add document example annotation to `MigrateBase64UtilsTest`

--- src/test/java/org/openrewrite/java/spring/framework/MigrateBase64UtilsTest.java
+++ src/test/java/org/openrewrite/java/spring/framework/MigrateBase64UtilsTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.spring.framework;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
@@ -33,6 +34,7 @@ class MigrateBase64UtilsTest implements RewriteTest {
             "spring-core-6.1.+"));
     }
 
+    @DocumentExample
     @Test
     void encodeDecode() {
         rewriteRun(
