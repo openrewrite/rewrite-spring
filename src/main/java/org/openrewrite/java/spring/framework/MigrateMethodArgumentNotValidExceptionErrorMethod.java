01a099ab2b86b3e2758471030381cbe74478bc34
chao.wang <chao.wang@zatech.com>
2025-04-03 18:18:50 +0800

    Add migration recipe for `MethodArgumentNotValidException` methods to use `BindErrorUtils`

---
+++ src/main/java/org/openrewrite/java/spring/framework/MigrateMethodArgumentNotValidExceptionErrorMethod.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring.framework;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+
+public class MigrateMethodArgumentNotValidExceptionErrorMethod extends Recipe {
+
+    private static final String TARGET_CLASS = "org.springframework.web.bind.MethodArgumentNotValidException";
+
+    private static final MethodMatcher ERRORS_TO_STRING_LIST = new MethodMatcher(TARGET_CLASS +
+        " errorsToStringList(java.util.List)");
+
+    private static final MethodMatcher ERRORS_TO_STRING_LIST_WITH_LOCALE = new MethodMatcher(TARGET_CLASS +
+        " errorsToStringList(java.util.List, org.springframework.context.MessageSource, java.util.Locale)");
+
+    private static final MethodMatcher RESOLVE_ERROR_MESSAGES = new MethodMatcher(TARGET_CLASS +
+        " resolveErrorMessages(org.springframework.context.MessageSource, java.util.Locale)");
+
+    @Override
+    public String getDisplayName() {
+        return "Migrate `org.springframework.web.bind.MethodArgumentNotValidException.errorsToStringList` and `resolveErrorMessages` method";
+    }
+
+    @Override
+    public String getDescription() {
+        return "`org.springframework.web.bind.MethodArgumentNotValidException.errorsToStringList` and `resolveErrorMessages` method was deprecated, in favor of `BindErrorUtils`.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(new UsesType<>(TARGET_CLASS, false), new JavaVisitor<ExecutionContext>() {
+
+            @Override
+            public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                J.MethodInvocation m = (J.MethodInvocation) super.visitMethodInvocation(method, ctx);
+                if (ERRORS_TO_STRING_LIST.matches(m)) {
+                    maybeAddImport("org.springframework.web.util.BindErrorUtils");
+                    return JavaTemplate.builder("BindErrorUtils.resolve(#{any()}).values().stream().toList()")
+                        .imports("org.springframework.web.util.BindErrorUtils")
+                        .javaParser(JavaParser.fromJavaVersion()
+                            .classpathFromResources(ctx, "spring-web-6.1.+"))
+                        .build()
+                        .apply(getCursor(), m.getCoordinates().replace(), m.getArguments().get(0));
+                }
+                if (ERRORS_TO_STRING_LIST_WITH_LOCALE.matches(m)) {
+                    maybeAddImport("org.springframework.web.util.BindErrorUtils");
+                    return JavaTemplate.builder("(#{any()} != null ?\n" +
+                            "\t\t\t\tBindErrorUtils.resolve(#{any()}, #{any()}, #{any()}).values().stream().toList() :\n" +
+                            "\t\t\t\tBindErrorUtils.resolve(#{any()}).values().stream().toList())")
+                        .imports("org.springframework.web.util.BindErrorUtils")
+                        .javaParser(JavaParser.fromJavaVersion()
+                            .classpathFromResources(ctx, "spring-web-6.1.+"))
+                        .build()
+                        .apply(getCursor(), m.getCoordinates().replace(), m.getArguments().get(1), m.getArguments().get(0),
+                            m.getArguments().get(1), m.getArguments().get(2), m.getArguments().get(0));
+                }
+                if (RESOLVE_ERROR_MESSAGES.matches(m)) {
+                    maybeAddImport("org.springframework.web.util.BindErrorUtils");
+                    return JavaTemplate.builder("BindErrorUtils.resolve(#{any()}.getAllErrors(), #{any()}, #{any()})")
+                        .imports("org.springframework.web.util.BindErrorUtils")
+                        .javaParser(JavaParser.fromJavaVersion()
+                            .classpathFromResources(ctx, "spring-web-6.1.+"))
+                        .build()
+                        .apply(getCursor(), m.getCoordinates().replace(), m.getSelect(), m.getArguments().get(0), m.getArguments().get(1));
+                }
+                return m;
+            }
+        });
+    }
+
+}
---
+++ src/test/java/org/openrewrite/java/spring/framework/MigrateMethodArgumentNotValidExceptionErrorMethodTest.java
@@ -0,0 +1,73 @@
+package org.openrewrite.java.spring.framework;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+import org.openrewrite.test.TypeValidation;
+
+import static org.openrewrite.java.Assertions.java;
+
+class MigrateMethodArgumentNotValidExceptionErrorMethodTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .typeValidationOptions(TypeValidation.none())
+          .recipe(new MigrateMethodArgumentNotValidExceptionErrorMethod())
+          .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(),
+            "spring-web-6.1.+", "spring-core", "spring-context"));
+    }
+
+    @DocumentExample
+    @Test
+    void migrateResourceHttpMessageWriterAddHeadersMethod() {
+        rewriteRun(
+          // language=java
+          java(
+            """
+              import org.springframework.context.MessageSource;
+              import java.util.Locale;
+              import org.springframework.web.bind.MethodArgumentNotValidException;
+              import java.util.List;
+              import java.util.Map;
+              import org.springframework.validation.BindException;
+              import org.springframework.validation.ObjectError;
+
+              class A {
+                  public void handleValidationError(BindException bindException, MethodArgumentNotValidException methodArgumentNotValidException, MessageSource messageSource, Locale locale) {
+                      List<ObjectError> errors = bindException.getAllErrors();
+                      List<String> errorMessages = MethodArgumentNotValidException.errorsToStringList(errors);
+                      List<String> errorMessages2 = MethodArgumentNotValidException.errorsToStringList(errors, null, Locale.CANADA);
+                      Map<ObjectError, String> errorMessages = methodArgumentNotValidException.resolveErrorMessages(messageSource, locale);
+                  }
+              }
+              """,
+            """
+              import org.springframework.context.MessageSource;
+              import java.util.Locale;
+              import org.springframework.web.bind.MethodArgumentNotValidException;
+              import org.springframework.web.util.BindErrorUtils;
+
+              import java.util.List;
+              import java.util.Map;
+              import org.springframework.validation.BindException;
+              import org.springframework.validation.ObjectError;
+
+              class A {
+                  public void handleValidationError(BindException bindException, MethodArgumentNotValidException methodArgumentNotValidException, MessageSource messageSource, Locale locale) {
+                      List<ObjectError> errors = bindException.getAllErrors();
+                      List<String> errorMessages = BindErrorUtils.resolve(errors).values().stream().toList();
+                      List<String> errorMessages2 = (null != null ?
+                              BindErrorUtils.resolve(errors, null, Locale.CANADA).values().stream().toList() :
+                              BindErrorUtils.resolve(errors).values().stream().toList());
+                      Map<ObjectError, String> errorMessages = BindErrorUtils.resolve(methodArgumentNotValidException.getAllErrors(), messageSource, locale);
+                  }
+              }
+              """
+          )
+        );
+    }
+}
