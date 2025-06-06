1b940fdd0bf832c464d2ebafae950c18a6a4e926
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2025-06-06 20:09:25 +0200

    Fix compile issue

--- src/testWithSpringBoot_3_4/java/org/openrewrite/java/spring/boot3/ConfigurationPropertiesDefaultValueTest.java
+++ src/testWithSpringBoot_3_4/java/org/openrewrite/java/spring/boot3/ConfigurationPropertiesDefaultValueTest.java
@@ -19,6 +19,8 @@ import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 
 import static org.openrewrite.java.Assertions.java;
+
+import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 import static org.openrewrite.java.Assertions.*;
