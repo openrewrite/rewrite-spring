d861cee2e6c1235296457e7227af6675b4cbb48e
Tim te Beek <tim@moderne.io>
2025-06-10 12:29:49 +0200

    Update ConfigurationPropertiesDefaultValueTest.java

--- src/testWithSpringBoot_3_4/java/org/openrewrite/java/spring/boot3/ConfigurationPropertiesDefaultValueTest.java
+++ src/testWithSpringBoot_3_4/java/org/openrewrite/java/spring/boot3/ConfigurationPropertiesDefaultValueTest.java
@@ -17,13 +17,11 @@ package org.openrewrite.java.spring.boot3;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
-
-import static org.openrewrite.java.Assertions.java;
-
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
-import static org.openrewrite.java.Assertions.*;
+
+import static org.openrewrite.java.Assertions.java;
 
 class ConfigurationPropertiesDefaultValueTest implements RewriteTest {
 
