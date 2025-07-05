2f29f583072bd89ecb68fed2d8295b3a90c45e57
Tim te Beek <tim@moderne.io>
2025-07-06 00:25:22 +0200

    Minimize implementation by reusing visitors

--- src/main/java/org/openrewrite/java/spring/data/MigrateQueryToNativeQuery.java
+++ src/main/java/org/openrewrite/java/spring/data/MigrateQueryToNativeQuery.java
@@ -15,24 +15,21 @@
  */
 package org.openrewrite.java.spring.data;
 
-import org.jspecify.annotations.Nullable;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.AnnotationMatcher;
 import org.openrewrite.java.ChangeType;
 import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.RemoveAnnotationAttribute;
 import org.openrewrite.java.search.UsesType;
-import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.trait.Annotated;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.Space;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class MigrateQueryToNativeQuery extends Recipe {
 
@@ -40,9 +37,7 @@ public class MigrateQueryToNativeQuery extends Recipe {
     private static final String DATA_JPA_NATIVE_QUERY_FQN = "org.springframework.data.jpa.repository.NativeQuery";
 
     private static final AnnotationMatcher DATA_JPA_QUERY_ANNOTATION_MATCHER =
-            new AnnotationMatcher("@" + DATA_JPA_QUERY_FQN);
-
-    private static final boolean NATIVE_QUERY_DEFAULT_VALUE = false;
+            new AnnotationMatcher("@" + DATA_JPA_QUERY_FQN + "(nativeQuery = true)");
 
     @Override
     public String getDisplayName() {
@@ -64,87 +59,40 @@ public class MigrateQueryToNativeQuery extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        // Inspiration for this recipe found in org.openrewrite.java.spring.NoRequestMappingAnnotation
         return Preconditions.check(
                 new UsesType<>(DATA_JPA_QUERY_FQN, false),
                 new JavaIsoVisitor<ExecutionContext>() {
                     @Override
                     public J.Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
-                        J.Annotation a = super.visitAnnotation(annotation, ctx);
-                        if (DATA_JPA_QUERY_ANNOTATION_MATCHER.matches(a) && getCursor().getParentOrThrow().getValue() instanceof J.MethodDeclaration) {
-                            Optional<J.Assignment> nativeQueryArg = findNativeQueryArgument(a);
-                            boolean isNativeQuery = nativeQueryArg
-                                    .map(assignment -> extractBooleanValue(assignment))
-                                    .orElse(NATIVE_QUERY_DEFAULT_VALUE);
-                            if (!isNativeQuery) {
-                                return a;
-                            } else {
-                                maybeRemoveImport(DATA_JPA_QUERY_FQN);
+                        J.Annotation an = super.visitAnnotation(annotation, ctx);
 
-                                // Remove the argument nativeQuery
-                                List<Expression> retainedArgs;
-                                if (nativeQueryArg.isPresent()) {
-                                    retainedArgs = a.getArguments().stream()
-                                            .filter(arg -> !nativeQueryArg.get().equals(arg))
-                                            .collect(Collectors.toList());
-                                } else {
-                                    retainedArgs = a.getArguments();
+                        if (!new Annotated.Matcher(DATA_JPA_QUERY_ANNOTATION_MATCHER)
+                                .get(getCursor())
+                                .filter(a -> a.getAttribute("nativeQuery")
+                                        .filter(l -> "true".equals(l.getString()))
+                                        .isPresent())
+                                .isPresent()) {
+                            return an;
+                        }
+
+                        an = (J.Annotation) new RemoveAnnotationAttribute(DATA_JPA_QUERY_FQN, "nativeQuery")
+                                .getVisitor()
+                                .visitNonNull(an, ctx, getCursor().getParentOrThrow());
+                        an = (J.Annotation) new ChangeType(DATA_JPA_QUERY_FQN, DATA_JPA_NATIVE_QUERY_FQN, false)
+                                .getVisitor().visitNonNull(an, ctx, getCursor().getParentOrThrow());
+                        maybeRemoveImport(DATA_JPA_QUERY_FQN);
+                        maybeAddImport(DATA_JPA_NATIVE_QUERY_FQN);
+                        if (an.getArguments() != null && an.getArguments().size() == 1) {
+                            an = an.withArguments(ListUtils.mapFirst(an.getArguments(), v -> {
+                                if (v instanceof J.Assignment &&
+                                        ((J.Assignment) v).getVariable() instanceof J.Identifier &&
+                                        "value".equals(((J.Identifier) ((J.Assignment) v).getVariable()).getSimpleName())) {
+                                    return ((J.Assignment) v).getAssignment().withPrefix(Space.EMPTY);
                                 }
-
-                                a = a.withArguments(retainedArgs);
-                            }
-
-                            // Change the Annotation Type => call recipe ChangeType
-                            maybeAddImport(DATA_JPA_NATIVE_QUERY_FQN);
-                            a = (J.Annotation) new ChangeType(DATA_JPA_QUERY_FQN, DATA_JPA_NATIVE_QUERY_FQN, false)
-                                    .getVisitor().visit(a, ctx, getCursor().getParentOrThrow());
-
-                            // Apply shorthand style if there is only one remaining argument named "value"
-                            if (a != null && a.getArguments() != null && a.getArguments().size() == 1) {
-                                a = a.withArguments(
-                                        a.getArguments().stream()
-                                                .map(arg -> hasArgument("value").test(arg) ? ((J.Assignment) arg).getAssignment().withPrefix(Space.EMPTY) : arg)
-                                                .collect(Collectors.toList())
-                                );
-                            }
+                                return v;
+                            }));
                         }
-                        return a != null ? a : annotation;
-                    }
-
-                    private Optional<J.Assignment> findNativeQueryArgument(J.Annotation annotation) {
-                        if (annotation.getArguments() == null) {
-                            return Optional.empty();
-                        }
-                        return annotation.getArguments().stream()
-                                .filter(hasArgument("nativeQuery"))
-                                .map(J.Assignment.class::cast)
-                                .findFirst();
-                    }
-
-                    private Predicate<Expression> hasArgument(String argName) {
-                        return arg -> arg instanceof J.Assignment &&
-                                ((J.Assignment) arg).getVariable() instanceof J.Identifier &&
-                                argName.equals(((J.Identifier) ((J.Assignment) arg).getVariable()).getSimpleName());
-                    }
-
-                    private @Nullable Boolean extractBooleanValue(J.@Nullable Assignment assignment) {
-                        if (assignment == null) {
-                            return null;
-                        }
-
-                        Expression assignedExpr = assignment.getAssignment();
-
-                        if (assignedExpr instanceof J.Literal) {
-                            J.Literal literal = (J.Literal) assignedExpr;
-                            Object value = literal.getValue();
-
-                            if (value instanceof Boolean) {
-                                return (Boolean) value;
-                            }
-                        }
-
-                        // If it's not a literal (e.g. a method call, variable, etc.), we can't resolve statically
-                        return null;
+                        return an;
                     }
                 }
         );
