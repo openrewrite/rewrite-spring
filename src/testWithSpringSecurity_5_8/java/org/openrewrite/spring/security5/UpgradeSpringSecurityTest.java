56838b339409a4ac753148bdf0f314cff792f59b
Jente Sondervorst <jentesondervorst@gmail.com>
2025-08-06 15:03:58 +0200

    There is not really a file existing for this composite recipe test

--- src/testWithSpringSecurity_5_8/java/org/openrewrite/spring/security5/UpgradeSpringSecurityTest.java
+++ src/testWithSpringSecurity_5_8/java/org/openrewrite/spring/security5/UpgradeSpringSecurityTest.java
@@ -16,7 +16,7 @@
 package org.openrewrite.spring.security5;
 
 import org.junit.jupiter.api.Test;
-import org.openrewrite.DocumentExample;
+import org.openrewrite.Issue;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -32,7 +32,7 @@ class UpgradeSpringSecurityTest implements RewriteTest {
     }
 
     @Test
-    @DocumentExample
+    @Issue("https://github.com/openrewrite/rewrite-spring/pull/757")
     void canBeUpgradedIfAuthenticationManagerBuilderConfigurationPresent() {
         rewriteRun(
           //language=java
