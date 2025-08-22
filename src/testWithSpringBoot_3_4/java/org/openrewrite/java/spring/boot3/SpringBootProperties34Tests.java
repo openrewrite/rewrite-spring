c4f44a6853064904d77336b20e81d0aad68df0c4
Tim te Beek <tim@moderne.io>
2025-08-22 19:35:11 +0200

    Use `recipeFromYaml`

--- src/testWithSpringBoot_3_4/java/org/openrewrite/java/spring/boot3/SpringBootProperties34Tests.java
+++ src/testWithSpringBoot_3_4/java/org/openrewrite/java/spring/boot3/SpringBootProperties34Tests.java
@@ -1,7 +1,6 @@
 package org.openrewrite.java.spring.boot3;
 
 import org.junit.jupiter.api.Test;
-import org.openrewrite.config.Environment;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
@@ -13,12 +12,30 @@ class SpringBootProperties34Test implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec
-          .expectedCyclesThatMakeChanges(1)
-          .recipe(Environment.builder()
-            .scanRuntimeClasspath("org.openrewrite.java.spring")
-            .build()
-            .activateRecipes("org.openrewrite.java.spring.boot3.SpringBootProperties_3_4_Test")
-          );
+          .recipeFromYaml(
+            //language=yaml
+            """
+              type: specs.openrewrite.org/v1beta/recipe
+              name: org.openrewrite.java.spring.boot3.SpringBootProperties_3_4_Test
+              displayName: Migrate Spring Boot properties to 3.4
+              description: Migrate properties found in `application.properties` and `application.yml`.
+              tags:
+                - spring
+                - boot
+              recipeList:
+                - org.openrewrite.java.spring.ChangeSpringPropertyValue:
+                    propertyKey: management.endpoint.auditevents.enabled
+                    newValue: read-only
+                    oldValue: true
+                - org.openrewrite.java.spring.ChangeSpringPropertyValue:
+                    propertyKey: management.endpoint.auditevents.enabled
+                    newValue: none
+                    oldValue: false
+                - org.openrewrite.java.spring.ChangeSpringPropertyKey:
+                    oldPropertyKey: management.endpoint.auditevents.enabled
+                    newPropertyKey: management.endpoint.auditevents.access
+              """,
+            "org.openrewrite.java.spring.boot3.SpringBootProperties_3_4_Test");
     }
 
     @Test
