6e78c004aa21179da6463dd784750c438df60fd5
Tim te Beek <tim@moderne.io>
2025-08-26 13:13:31 +0200

    Add missing import

--- src/testWithSpringBoot_3_4/java/org/openrewrite/java/spring/boot3/MigrateEndpointAccessValueSpring34Test.java
+++ src/testWithSpringBoot_3_4/java/org/openrewrite/java/spring/boot3/MigrateEndpointAccessValueSpring34Test.java
@@ -23,6 +23,7 @@ import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static java.util.Collections.emptyList;
+import static org.openrewrite.properties.Assertions.properties;
 import static org.openrewrite.yaml.Assertions.yaml;
 
 class MigrateEndpointAccessValueSpring34Test implements RewriteTest {
