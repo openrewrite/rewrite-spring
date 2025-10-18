6e1b2edc4a41ae96cc262f153f1e46b4b5471ab1
Ishaan Sharma <ishaansharma5567@gmail.com>
2025-10-18 18:57:18 -0400

    Separating application.properties into different files based on profile (#563)
    * First draft of changes
    
    * Added test to ensure application.properties doesn't change if no additional profiles are specified
    
    * Used Auto Formatter
    
    * More Formatting
    
    * More Formatting 2
    
    * fixing edge case 1
    
    * unable to append to existing application-prof.properties
    
    * testing for no application.properties
    
    * removed cycles from tests
    
    * This code works!
    
    * Forgot to format test file
    
    * More formatting
    
    * Fixed weird spacing issue
    
    * Formatting and Code Cleanup
    
    * Changed approach to create blank properties files and append to everything in order to simplify code
    
    * Converted existingPropertiesFiles to Set of Strings
    
    * New properties files will go into same folder as application.properties
    
    Finds properties files that are within folders
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Add missing braces, language hints and apply formatter
    
    * Minor polish
    
    * Add test showing multi module project structure
    
    * Works for multi-modular projects
    
    * responding to bot's comment
    
    * using JavaProject
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Move test class & method
    
    * Fix compilation
    
    * Use Paths in Accumulator object
    
    * Minimize
    
    * Final polish
    
    * Add to best practices
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/test/java/org/openrewrite/java/spring/SeparateApplicationPropertiesByProfileTest.java
@@ -0,0 +1,278 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.spring;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.mavenProject;
+import static org.openrewrite.java.Assertions.srcMainResources;
+import static org.openrewrite.properties.Assertions.properties;
+
+class SeparateApplicationPropertiesByProfileTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new SeparateApplicationPropertiesByProfile());
+    }
+
+    @DocumentExample
+    @Test
+    void separateProfileWithAppend() {
+        rewriteRun(
+          mavenProject("parent",
+            srcMainResources(
+              properties(
+                //language=properties
+                """
+                  line1=line1
+                  """,
+                //language=properties
+                """
+                  line1=line1
+                  oauth2.clientId=9999999999999999999999
+                  service.domainUrl= https://this.is.my.dev.url.com
+                  app.config.currentEnvironment=DEV
+                  """,
+                sourceSpecs -> sourceSpecs.path("application-dev.properties")
+              ),
+              properties(
+                //language=properties
+                """
+                  spring.application.name=Openrewrite-PR-Service
+                  #PR-Service
+                  base-url.PR-services=http://my.url.com
+                  exchange-token=1234567890
+                  exchange-tokens=${base-url.PR-services}/exchange-token
+                  !---
+                  spring.config.activate.on-profile=dev
+                  oauth2.clientId=9999999999999999999999
+                  service.domainUrl= https://this.is.my.dev.url.com
+                  app.config.currentEnvironment=DEV
+                  #---
+                  spring.config.activate.on-profile=local
+                  app.config.currentEnvironment=LOCAL
+
+
+                  #---
+                  #### XX Configuration ####
+                  spring.config.activate.on-profile=prod
+                  oauth2.clientId=77777777777777
+                  service.domainUrl=https://this.is.my.prod.url.com
+                  """,
+                //language=properties
+                """
+                  spring.application.name=Openrewrite-PR-Service
+                  #PR-Service
+                  base-url.PR-services=http://my.url.com
+                  exchange-token=1234567890
+                  exchange-tokens=${base-url.PR-services}/exchange-token
+                  """,
+                sourceSpecs -> sourceSpecs.path("application.properties")
+              ),
+              properties(
+                doesNotExist(),
+                """
+                  app.config.currentEnvironment=LOCAL
+                  """,
+                sourceSpecs -> sourceSpecs.path("application-local.properties")
+              ),
+              properties(
+                doesNotExist(),
+                //language=properties
+                """
+                  #### XX Configuration ####
+                  oauth2.clientId=77777777777777
+                  service.domainUrl=https://this.is.my.prod.url.com
+                  """,
+                sourceSpecs -> sourceSpecs.path("application-prod.properties")
+              )
+            )
+          )
+        );
+    }
+
+    @Test
+    void noApplicationProperties() {
+        rewriteRun(
+          mavenProject("parent",
+            srcMainResources(
+              properties(
+                //language=properties
+                """
+                  spring.application.name=Openrewrite-PR-Service
+                  #PR-Service
+
+                  base-url.PR-services=http://my.url.com
+                  exchange-token=1234567890
+                  exchange-tokens=${base-url.PR-services}/exchange-token
+                  """,
+                sourceSpecs -> sourceSpecs.path("application-dev.properties"))
+            )
+          )
+        );
+    }
+
+    @Test
+    void noSeparateProfile() {
+        rewriteRun(
+          mavenProject("parent",
+            srcMainResources(
+              properties(
+                //language=properties
+                """
+                  spring.application.name=Openrewrite-PR-Service
+                  #PR-Service
+
+                  base-url.PR-services=http://my.url.com
+                  exchange-token=1234567890
+                  exchange-tokens=${base-url.PR-services}/exchange-token
+                  """,
+                sourceSpecs -> sourceSpecs.path("application.properties"))
+            )
+          )
+        );
+    }
+
+    @Test
+    void separateProfileWithoutAppend() {
+        rewriteRun(
+          mavenProject("parent",
+            srcMainResources(
+              properties(
+                doesNotExist(),
+                //language=properties
+                """
+                  oauth2.clientId=9999999999999999999999
+                  service.domainUrl= https://this.is.my.dev.url.com
+                  app.config.currentEnvironment=DEV
+                  """,
+                sourceSpecs -> sourceSpecs.path("application-dev.properties")
+              ),
+              properties(
+                //language=properties
+                """
+                  spring.application.name=Openrewrite-PR-Service
+                  #PR-Service
+                  base-url.PR-services=http://my.url.com
+                  exchange-token=1234567890
+                  exchange-tokens=${base-url.PR-services}/exchange-token
+                  !---
+                  spring.config.activate.on-profile=dev
+                  oauth2.clientId=9999999999999999999999
+                  service.domainUrl= https://this.is.my.dev.url.com
+                  app.config.currentEnvironment=DEV
+                  #---
+                  spring.config.activate.on-profile=local
+                  app.config.currentEnvironment=LOCAL
+
+
+                  #---
+                  #### XX Configuration ####
+                  spring.config.activate.on-profile=prod
+                  oauth2.clientId=77777777777777
+                  service.domainUrl=https://this.is.my.prod.url.com
+                  """,
+                //language=properties
+                """
+                  spring.application.name=Openrewrite-PR-Service
+                  #PR-Service
+                  base-url.PR-services=http://my.url.com
+                  exchange-token=1234567890
+                  exchange-tokens=${base-url.PR-services}/exchange-token
+                  """,
+                sourceSpecs -> sourceSpecs.path("application.properties")
+              ),
+              properties(
+                doesNotExist(),
+                //language=properties
+                """
+                  app.config.currentEnvironment=LOCAL
+                  """,
+                sourceSpecs -> sourceSpecs.path("application-local.properties")
+              ),
+              properties(
+                doesNotExist(),
+                //language=properties
+                """
+                  #### XX Configuration ####
+                  oauth2.clientId=77777777777777
+                  service.domainUrl=https://this.is.my.prod.url.com
+                  """,
+                sourceSpecs -> sourceSpecs.path("application-prod.properties")
+              )
+            )
+          )
+        );
+    }
+
+    @Test
+    void multiModuleProject() {
+        rewriteRun(
+          mavenProject("parent",
+            mavenProject("service",
+              srcMainResources(
+                properties(
+                  """
+                    global.service=true
+                    !---
+                    spring.config.activate.on-profile=dev
+                    dev.service=true
+                    """,
+                  """
+                    global.service=true
+                    """,
+                  spec -> spec.path("application.properties")
+                ),
+                properties(
+                  doesNotExist(),
+                  """
+                    dev.service=true
+                    """,
+                  spec -> spec.path("application-dev.properties")
+                )
+              )
+            ),
+            mavenProject("client",
+              srcMainResources(
+                properties("""
+                    global.client=true
+                    !---
+                    spring.config.activate.on-profile=dev
+                    dev.client=true
+                    """,
+                  """
+                    global.client=true
+                    """,
+                  spec -> spec.path("application.properties")
+                ),
+                properties(
+                  """
+                    dev.existing=true
+                    """,
+                  """
+                    dev.existing=true
+                    dev.client=true
+                    """,
+                  spec -> spec.path("application-dev.properties")
+                )
+              )
+            )
+          )
+        );
+    }
+}
