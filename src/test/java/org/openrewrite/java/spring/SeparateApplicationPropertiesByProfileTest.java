c585afca113b4b870c7ccde12867f2aa147e22f9
Tim te Beek <tim@moderne.io>
2025-10-18 23:33:08 +0200

    Fix compilation

--- src/test/java/org/openrewrite/java/spring/SeparateApplicationPropertiesByProfileTest.java
+++ src/test/java/org/openrewrite/java/spring/SeparateApplicationPropertiesByProfileTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2024 the original author or authors.
+ * Copyright 2025 the original author or authors.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Moderne Source Available License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- * https://www.apache.org/licenses/LICENSE-2.0
+ * https://docs.moderne.io/licensing/moderne-source-available-license
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -86,7 +86,7 @@ class SeparateApplicationPropertiesByProfileTest implements RewriteTest {
               ),
               properties(
                 doesNotExist(),
-                """,
+                """
                   app.config.currentEnvironment=LOCAL
                   """,
                 sourceSpecs -> sourceSpecs.path("application-local.properties")
