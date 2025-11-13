6404c22172189eefd1fc46fba2b7d78da0662e17
Tim te Beek <tim@moderne.io>
2025-11-13 11:24:52 +0100

    Move SpringFox recipes out of SpringDoc package (#828)

--- src/main/java/org/openrewrite/java/spring/doc/ApiInfoBuilderToInfo.java
+++ src/main/java/org/openrewrite/java/spring/doc/ApiInfoBuilderToInfo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.springdoc;
+package org.openrewrite.java.spring.doc;
 
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
