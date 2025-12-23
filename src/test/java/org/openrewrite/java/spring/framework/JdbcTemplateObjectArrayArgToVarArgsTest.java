000c20043f5cbd8d830dbc0c213b1accdd68356e
Tim te Beek <tim@moderne.io>
2025-12-23 10:16:17 +0100

    Restore original header

--- src/test/java/org/openrewrite/java/spring/framework/JdbcTemplateObjectArrayArgToVarArgsTest.java
+++ src/test/java/org/openrewrite/java/spring/framework/JdbcTemplateObjectArrayArgToVarArgsTest.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2024 the original author or authors.
- *
+ * Copyright 2025 the original author or authors.
+ * <p>
  * Licensed under the Moderne Source Available License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * https://docs.moderne.io/licensing/moderne-source-available-license
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
