7464f6d0cf07515935727b7e8c7758f66c3d8782
Tim te Beek <tim@moderne.io>
2026-01-19 17:10:16 +0100

    Adopt `AnnotationService`

--- src/main/java/org/openrewrite/java/spring/boot4/AddAutoConfigureTestRestTemplate.java
+++ src/main/java/org/openrewrite/java/spring/boot4/AddAutoConfigureTestRestTemplate.java
@@ -25,41 +25,44 @@ import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.service.AnnotationService;
 import org.openrewrite.java.tree.J;
 
-import java.util.Comparator;
+import static java.util.Comparator.comparing;
 
 public class AddAutoConfigureTestRestTemplate extends Recipe {
 
-    private static final String FQN_TESTRESTTEMPLATE = "org.springframework.boot.resttestclient.TestRestTemplate";
-    private static final String AUTOCONFIGURE_TESTRESTTEMPLATE_SIMPLE_NAME = "AutoConfigureTestRestTemplate";
-    private static final String FQN_AUTOCONFIGURE_TESTRESTTEMPLATE = "org.springframework.boot.resttestclient.autoconfigure." + AUTOCONFIGURE_TESTRESTTEMPLATE_SIMPLE_NAME;
+    private static final String SIMPLE_NAME = "AutoConfigureTestRestTemplate";
+    private static final String FULLY_QUALIFIED = "org.springframework.boot.resttestclient.autoconfigure." + SIMPLE_NAME;
+    private static final String TEST_REST_TEMPLATE = "org.springframework.boot.resttestclient.TestRestTemplate";
 
-    private static final AnnotationMatcher SPRING_BOOT_TEST_MATCHER = new AnnotationMatcher("@org.springframework.boot.test.context.SpringBootTest", true);
-    private static final AnnotationMatcher AUTOCONFIGURE_TESTRESTTEMPLATE_MATCHER = new AnnotationMatcher("@" + FQN_AUTOCONFIGURE_TESTRESTTEMPLATE, true);
+    private static final AnnotationMatcher AUTO_CONFIGURE_TEST_REST_TEMPLATE = new AnnotationMatcher(FULLY_QUALIFIED, true);
+    private static final AnnotationMatcher SPRING_BOOT_TEST = new AnnotationMatcher("@org.springframework.boot.test.context.SpringBootTest", true);
 
     @Getter
     final String displayName = "Add `@AutoConfigureTestRestTemplate` if necessary";
 
     @Getter
-    final String description = "Adds `@AutoConfigureTestRestTemplate` to test classes annotated with `@SpringBootTest` that use `TestRestTemplate` since this bean is no longer auto-configured as described in the [Spring Boot 4 migration guide](https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-4.0-Migration-Guide#using-webclient-or-testresttemplate-and-springboottest).";
+    final String description = "Adds `@AutoConfigureTestRestTemplate` to test classes annotated with " +
+            "`@SpringBootTest` that use `TestRestTemplate` since this bean is no longer auto-configured as described " +
+            "in the [Spring Boot 4 migration guide](https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-4.0-Migration-Guide#using-webclient-or-testresttemplate-and-springboottest).";
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return Preconditions.check(new UsesType<>(FQN_TESTRESTTEMPLATE, true), new JavaIsoVisitor<ExecutionContext>() {
+        return Preconditions.check(new UsesType<>(TEST_REST_TEMPLATE, true), new JavaIsoVisitor<ExecutionContext>() {
             @Override
             public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
                 J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
 
-                if (cd.getLeadingAnnotations().stream().anyMatch(SPRING_BOOT_TEST_MATCHER::matches) &&
-                        cd.getLeadingAnnotations().stream().noneMatch(AUTOCONFIGURE_TESTRESTTEMPLATE_MATCHER::matches)) {
-
-                    maybeAddImport(FQN_AUTOCONFIGURE_TESTRESTTEMPLATE);
-                    return JavaTemplate.builder("@" + AUTOCONFIGURE_TESTRESTTEMPLATE_SIMPLE_NAME)
-                            .imports(FQN_AUTOCONFIGURE_TESTRESTTEMPLATE)
-                            .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "spring-boot-resttestclient-4.+"))
+                AnnotationService annotationService = service(AnnotationService.class);
+                if (annotationService.matches(getCursor(), SPRING_BOOT_TEST) &&
+                        !annotationService.matches(getCursor(), AUTO_CONFIGURE_TEST_REST_TEMPLATE)) {
+                    maybeAddImport(FULLY_QUALIFIED);
+                    return JavaTemplate.builder("@" + SIMPLE_NAME)
+                            .imports(FULLY_QUALIFIED)
+                            .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "spring-boot-resttestclient-4"))
                             .build()
-                            .apply(getCursor(), cd.getCoordinates().addAnnotation(Comparator.comparing(J.Annotation::getSimpleName)));
+                            .apply(updateCursor(cd), cd.getCoordinates().addAnnotation(comparing(J.Annotation::getSimpleName)));
                 }
 
                 return cd;
