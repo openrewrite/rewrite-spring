208b8b2decc969a5192f576442e9021324df6428
Tim te Beek <tim@moderne.io>
2026-01-27 15:05:04 +0100

    Slight polish

--- src/main/java/org/openrewrite/java/spring/NoHttpExchangeAnnotation.java
+++ src/main/java/org/openrewrite/java/spring/NoHttpExchangeAnnotation.java
@@ -22,6 +22,7 @@ import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.internal.ListUtils;
+import org.openrewrite.internal.StringUtils;
 import org.openrewrite.java.AnnotationMatcher;
 import org.openrewrite.java.ChangeType;
 import org.openrewrite.java.JavaIsoVisitor;
@@ -46,6 +47,8 @@ import java.util.Optional;
 
 public class NoHttpExchangeAnnotation extends Recipe {
 
+    private static final AnnotationMatcher HTTP_EXCHANGE_ANNOTATION_MATCHER = new AnnotationMatcher("@org.springframework.web.service.annotation.HttpExchange");
+
     @Getter
     final String displayName = "Remove `HttpExchange` annotations";
 
@@ -56,92 +59,81 @@ public class NoHttpExchangeAnnotation extends Recipe {
     public TreeVisitor<?, ExecutionContext> getVisitor() {
         return Preconditions.check(
                 new UsesType<>("org.springframework.web.service.annotation.HttpExchange", false),
-                new NoHttpExchangeAnnotationVisitor());
-    }
+                new JavaIsoVisitor<ExecutionContext>() {
+                    @Override
+                    public J.Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
+                        J.Annotation a = super.visitAnnotation(annotation, ctx);
+                        if (HTTP_EXCHANGE_ANNOTATION_MATCHER.matches(a) &&
+                                getCursor().getParentOrThrow().getValue() instanceof J.MethodDeclaration) {
+                            Optional<J.Assignment> methodArg = findMethodArgument(a);
+                            String targetAnnotationClassName = methodArg
+                                    .map(this::extractHttpMethod)
+                                    .map(this::toExchangeAnnotation)
+                                    .orElse(null);
+                            if (targetAnnotationClassName == null) {
+                                return a;
+                            }
 
-    private static class NoHttpExchangeAnnotationVisitor extends JavaIsoVisitor<ExecutionContext> {
-        private static final AnnotationMatcher HTTP_EXCHANGE_ANNOTATION_MATCHER = new AnnotationMatcher("@org.springframework.web.service.annotation.HttpExchange");
+                            maybeRemoveImport("org.springframework.web.service.annotation.HttpExchange");
+                            maybeAddImport("org.springframework.web.service.annotation." + targetAnnotationClassName);
 
-        @Override
-        public J.Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
-            J.Annotation a = super.visitAnnotation(annotation, ctx);
-            if (HTTP_EXCHANGE_ANNOTATION_MATCHER.matches(a) && getCursor().getParentOrThrow().getValue() instanceof J.MethodDeclaration) {
-                Optional<J.Assignment> methodArg = findMethodArgument(a);
-                Optional<String> httpMethod = methodArg.map(this::extractHttpMethod);
-                String targetAnnotationClassName = httpMethod.map(this::toExchangeAnnotation).orElse(null);
+                            a = a.withArguments(ListUtils.filter(a.getArguments(), arg -> !methodArg.get().equals(arg)));
+                            a = (J.Annotation) new ChangeType(
+                                    "org.springframework.web.service.annotation.HttpExchange",
+                                    "org.springframework.web.service.annotation." + targetAnnotationClassName,
+                                    false
+                            ).getVisitor().visit(a, ctx, getCursor().getParentOrThrow());
 
-                if (targetAnnotationClassName == null) {
-                    return a;
-                }
-
-                maybeRemoveImport("org.springframework.web.service.annotation.HttpExchange");
-
-                if (a.getArguments() != null) {
-                    a = a.withArguments(ListUtils.map(a.getArguments(), arg ->
-                            methodArg.get().equals(arg) ? null : arg));
-                }
-
-                maybeAddImport("org.springframework.web.service.annotation." + targetAnnotationClassName);
-                a = (J.Annotation) new ChangeType(
-                        "org.springframework.web.service.annotation.HttpExchange",
-                        "org.springframework.web.service.annotation." + targetAnnotationClassName,
-                        false
-                ).getVisitor().visit(a, ctx, getCursor().getParentOrThrow());
-
-                if (a != null && a.getArguments() != null && a.getArguments().size() == 1) {
-                    a = a.withArguments(ListUtils.map(a.getArguments(), arg -> {
-                        if (arg instanceof J.Assignment && ((J.Assignment) arg).getVariable() instanceof J.Identifier) {
-                            J.Identifier ident = (J.Identifier) ((J.Assignment) arg).getVariable();
-                            if ("value".equals(ident.getSimpleName()) || "url".equals(ident.getSimpleName())) {
-                                return ((J.Assignment) arg).getAssignment().withPrefix(Space.EMPTY);
+                            if (a != null && a.getArguments() != null && a.getArguments().size() == 1) {
+                                a = a.withArguments(ListUtils.map(a.getArguments(), arg -> {
+                                    if (arg instanceof J.Assignment && ((J.Assignment) arg).getVariable() instanceof J.Identifier) {
+                                        J.Identifier ident = (J.Identifier) ((J.Assignment) arg).getVariable();
+                                        if ("value".equals(ident.getSimpleName()) || "url".equals(ident.getSimpleName())) {
+                                            return ((J.Assignment) arg).getAssignment().withPrefix(Space.EMPTY);
+                                        }
+                                    }
+                                    return arg;
+                                }));
                             }
                         }
-                        return arg;
-                    }));
-                }
-            }
-            return a != null ? a : annotation;
-        }
+                        return a != null ? a : annotation;
+                    }
 
-        private Optional<J.Assignment> findMethodArgument(J.Annotation annotation) {
-            if (annotation.getArguments() == null) {
-                return Optional.empty();
-            }
+                    private Optional<J.Assignment> findMethodArgument(J.Annotation annotation) {
+                        if (annotation.getArguments() == null) {
+                            return Optional.empty();
+                        }
 
-            return annotation.getArguments().stream()
-                    .filter(arg -> arg instanceof J.Assignment &&
-                            ((J.Assignment) arg).getVariable() instanceof J.Identifier &&
-                            "method".equals(((J.Identifier) ((J.Assignment) arg).getVariable()).getSimpleName()))
-                    .map(J.Assignment.class::cast)
-                    .findFirst();
-        }
+                        return annotation.getArguments().stream()
+                                .filter(arg -> arg instanceof J.Assignment &&
+                                        ((J.Assignment) arg).getVariable() instanceof J.Identifier &&
+                                        "method".equals(((J.Identifier) ((J.Assignment) arg).getVariable()).getSimpleName()))
+                                .map(J.Assignment.class::cast)
+                                .findFirst();
+                    }
 
-        private @Nullable String extractHttpMethod(J.@Nullable Assignment assignment) {
-            if (assignment == null) {
-                return null;
-            }
+                    private @Nullable String extractHttpMethod(J.@Nullable Assignment assignment) {
+                        if (assignment != null && assignment.getAssignment() instanceof J.Literal) {
+                            Object value = ((J.Literal) assignment.getAssignment()).getValue();
+                            if (value instanceof String) {
+                                return (String) value;
+                            }
+                        }
+                        return null;
+                    }
 
-            if (assignment.getAssignment() instanceof J.Literal) {
-                Object value = ((J.Literal) assignment.getAssignment()).getValue();
-                if (value instanceof String) {
-                    return (String) value;
-                }
-            }
-
-            return null;
-        }
-
-        private @Nullable String toExchangeAnnotation(String method) {
-            switch (method) {
-                case "GET":
-                case "POST":
-                case "PUT":
-                case "PATCH":
-                case "DELETE":
-                    return method.charAt(0) + method.toLowerCase().substring(1) + "Exchange";
-                default:
-                    return null;
-            }
-        }
+                    private @Nullable String toExchangeAnnotation(String method) {
+                        switch (method) {
+                            case "GET":
+                            case "POST":
+                            case "PUT":
+                            case "PATCH":
+                            case "DELETE":
+                                return StringUtils.capitalize(method.toLowerCase()) + "Exchange";
+                            default:
+                                return null;
+                        }
+                    }
+                });
     }
 }
