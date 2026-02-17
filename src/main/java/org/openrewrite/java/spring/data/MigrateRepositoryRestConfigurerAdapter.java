368a0b202acc28692d6b8d7e5670498ecf8a7954
Tim te Beek <tim@moderne.io>
2026-02-17 21:43:01 +0100

    Move to spring data package

--- src/main/java/org/openrewrite/java/spring/data/MigrateRepositoryRestConfigurerAdapter.java
+++ src/main/java/org/openrewrite/java/spring/data/MigrateRepositoryRestConfigurerAdapter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.spring.framework;
+package org.openrewrite.java.spring.data;
 
 import lombok.Getter;
 import org.jspecify.annotations.Nullable;
--- src/test/java/org/openrewrite/java/spring/data/MigrateRepositoryRestConfigurerAdapterTest.java
+++ src/test/java/org/openrewrite/java/spring/data/MigrateRepositoryRestConfigurerAdapterTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.spring.framework;
+package org.openrewrite.java.spring.data;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
