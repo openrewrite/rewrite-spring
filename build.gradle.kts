f442e97f213a9fe7b9f0eebe5a74eb7aa358e429
Steve Elliott <steve.aom.elliott@gmail.com>
2026-04-30 18:19:29 +0000

    Java 25 on Gradle 9.5.0
    Use this link to re-run the recipe: https://app.moderne.io/builder/J87rq3T51?organizationId=QUxML01vZGVybmUvTW9kZXJuZSArIE9wZW5SZXdyaXRl
    
    Co-authored-by: Moderne <team@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -208,7 +208,7 @@ dependencies {
     implementation("org.openrewrite.recipe:rewrite-static-analysis:${rewriteVersion}")
     implementation("org.openrewrite.gradle.tooling:model:${rewriteVersion}")
 
-    runtimeOnly("org.openrewrite:rewrite-java-21")
+    runtimeOnly("org.openrewrite:rewrite-java-25")
     runtimeOnly("org.openrewrite.recipe:rewrite-apache:$rewriteVersion")
     runtimeOnly("org.openrewrite.recipe:rewrite-hibernate:$rewriteVersion")
     runtimeOnly("org.openrewrite.recipe:rewrite-jackson:${rewriteVersion}")
@@ -226,7 +226,7 @@ dependencies {
 
     // for generating properties migration configurations
     testImplementation("io.github.classgraph:classgraph:latest.release")
-    testImplementation("org.openrewrite:rewrite-java-21")
+    testImplementation("org.openrewrite:rewrite-java-25")
     testImplementation("org.openrewrite:rewrite-kotlin")
     testImplementation("org.openrewrite.recipe:rewrite-migrate-java:$rewriteVersion")
     testImplementation("org.openrewrite.recipe:rewrite-testing-frameworks:$rewriteVersion")
