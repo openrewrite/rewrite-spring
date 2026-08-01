1552b3aa647bf994b70abed46d24466c6cf639da
Marius Barbulescu <marius.barbulescu@gmail.com>
2026-08-01 20:01:43 +0200

    moved AddParametersCompilerFlagToMaven (recipe + test) from org.openrewrite.java.spring to org.openrewrite.maven.spring

--- src/main/java/org/openrewrite/maven/spring/AddParametersCompilerFlagToMaven.java
+++ src/main/java/org/openrewrite/maven/spring/AddParametersCompilerFlagToMaven.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.spring;
+package org.openrewrite.maven.spring;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.EqualsAndHashCode;
